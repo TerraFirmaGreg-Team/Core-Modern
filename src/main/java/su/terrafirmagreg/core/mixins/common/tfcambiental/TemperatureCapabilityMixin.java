5de7f973e5c00c71b2e8651a75de1949b6321e41
Pyritie <Pyritie@gmail.com>
2025-03-31 02:29:54 +0100

    Added comment

--- src/main/java/su/terrafirmagreg/core/mixins/common/tfcambiental/TemperatureCapabilityMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/tfcambiental/TemperatureCapabilityMixin.java
@@ -1,10 +1,8 @@
 package su.terrafirmagreg.core.mixins.common.tfcambiental;
 
-import com.lumintorious.tfcambiental.TFCAmbientalConfig;
 import com.lumintorious.tfcambiental.api.*;
 import com.lumintorious.tfcambiental.capability.TemperatureCapability;
 import com.lumintorious.tfcambiental.modifier.TempModifierStorage;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,6 +11,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import su.terrafirmagreg.core.compat.tfcambiental.TFCAmbientalCompat;
 
+/**
+ * Adds support for 'fully insulated' armors that ignore tfc-ambiental.
+ * Mostly used for the lava diving suit, space suits, and late game armor
+ */
 @Mixin(value = TemperatureCapability.class, remap = false)
 public abstract class TemperatureCapabilityMixin {
 
@@ -31,7 +33,6 @@ public abstract class TemperatureCapabilityMixin {
 	@Shadow
 	private float potency;
 
-	//@SuppressWarnings("UnnecessaryReturnStatement")
 	@Inject(method = "evaluateModifiers", at = @At(value = "TAIL"))
 	public void tfg$evaluateModifiers(CallbackInfo ci)
 	{
