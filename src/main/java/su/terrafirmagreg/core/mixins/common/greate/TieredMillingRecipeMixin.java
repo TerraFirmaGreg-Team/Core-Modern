5f6649d77593b6bdc287bc871ef1ba21ac77a774
Pyritie <Pyritie@gmail.com>
2025-04-11 18:28:57 +0100

    Mixins to adjust max input/output counts of greate/VI recipes

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/greate/TieredMillingRecipeMixin.java
@@ -0,0 +1,17 @@
+package su.terrafirmagreg.core.mixins.common.greate;
+
+import electrolyte.greate.content.kinetics.millstone.TieredMillingRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = TieredMillingRecipe.class, remap = false)
+public class TieredMillingRecipeMixin {
+
+	@Inject(method = "getMaxOutputCount", at = @At(value = "HEAD"), cancellable = true)
+	private void tfg$getMaxOutputCount(CallbackInfoReturnable<Integer> cir)
+	{
+		cir.setReturnValue(9);
+	}
+}
