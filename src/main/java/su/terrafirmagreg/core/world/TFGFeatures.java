621c2d0d54be312c93087480d9c8ef2b5161046d
Pyritie <pyritie@gmail.com>
2025-05-13 17:50:08 +0100

    added features for dead coral

--- src/main/java/su/terrafirmagreg/core/world/TFGFeatures.java
+++ src/main/java/su/terrafirmagreg/core/world/TFGFeatures.java
@@ -15,6 +15,9 @@ public class TFGFeatures {
 	public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(Registries.FEATURE, TFGCore.MOD_ID);
 
 	public static final RegistryObject<LunarChorusPlantFeature> LUNAR_CHORUS_PLANT = register("lunar_chorus_plant", LunarChorusPlantFeature::new, NoneFeatureConfiguration.CODEC);
+	public static final RegistryObject<DeadCoralClawFeature> DEAD_CORAL_CLAW = register("dead_coral_claw", DeadCoralClawFeature::new, NoneFeatureConfiguration.CODEC);
+	public static final RegistryObject<DeadCoralMushroomFeature> DEAD_CORAL_MUSHROOM = register("dead_coral_mushroom", DeadCoralMushroomFeature::new, NoneFeatureConfiguration.CODEC);
+	public static final RegistryObject<DeadCoralTreeFeature> DEAD_CORAL_TREE = register("dead_coral_tree", DeadCoralTreeFeature::new, NoneFeatureConfiguration.CODEC);
 
 
 	private static <C extends FeatureConfiguration, F extends Feature<C>> RegistryObject<F> register(String name, Function<Codec<C>, F> factory, Codec<C> codec)
