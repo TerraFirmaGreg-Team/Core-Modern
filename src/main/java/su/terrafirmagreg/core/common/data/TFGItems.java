11e5d0565983cb3cdf464a0bf8836869ee2fdcd6
Pyritie <pyritie@gmail.com>
2025-05-27 22:13:53 +0100

    Release: 0.6.6 (#39)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Redeix <59435925+Redeix@users.noreply.github.com>
    Co-authored-by: Redeix <brayden.j.m.ford@gmail.com>
    Co-authored-by: Xikaro <os.valerievich@ya.ru>
    Co-authored-by: Thomasx0 <Thomas.lets.play@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/main/java/su/terrafirmagreg/core/common/data/TFGItems.java
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGItems.java
@@ -5,6 +5,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import su.terrafirmagreg.core.TFGCore;
+import su.terrafirmagreg.core.common.data.items.PiglinDisguise;
 
 import java.util.Locale;
 import java.util.function.Supplier;
@@ -19,6 +20,8 @@ public class TFGItems {
 
 	//public static final RegistryObject<Item> EXAMPLE_ITEM = register("pyritie");
 
+	public static final RegistryObject<Item> PIGLIN_DISGUISE =
+			ITEMS.register("piglin_disguise", () -> new PiglinDisguise(TFGBlocks.PIGLIN_DISGUISE_BLOCK.get(), new Item.Properties()));
 
 	private static RegistryObject<Item> register(String name)
 	{
