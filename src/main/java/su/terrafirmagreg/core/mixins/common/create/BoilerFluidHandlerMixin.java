fc78df4ddbae1a82b5e4e41719c9b4ed6c1e7a38
Zeropol <28099824+Zeropol@users.noreply.github.com>
2025-05-30 01:06:08 +0200

    Make Create boiler accept Gregtech Steam
    Edited src\main\java\su\terrafirmagreg\core\mixins\common\create\BoilerFluidHandlerMixin.java
    
    Beware, net.minecraft.world.level.material.Fluid.is(TagKey<Fluid> pTag) is deprecated, but it dit not work with ==

--- src/main/java/su/terrafirmagreg/core/mixins/common/create/BoilerFluidHandlerMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/create/BoilerFluidHandlerMixin.java
@@ -1,5 +1,6 @@
 package su.terrafirmagreg.core.mixins.common.create;
 
+import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.simibubi.create.content.fluids.tank.BoilerData.BoilerFluidHandler;
 import com.simibubi.create.foundation.fluid.FluidHelper;
 import net.dries007.tfc.common.fluids.TFCFluids;
@@ -11,6 +12,8 @@ import org.spongepowered.asm.mixin.Overwrite;
 public class BoilerFluidHandlerMixin {
     @Overwrite
     public boolean isFluidValid(int tank, FluidStack stack){    
-      return FluidHelper.isWater(stack.getFluid()) || (TFCFluids.RIVER_WATER.get() == stack.getFluid());
+      return FluidHelper.isWater(stack.getFluid())
+      || (TFCFluids.RIVER_WATER.get() == stack.getFluid())
+      || stack.getFluid().is(GTMaterials.Steam.getFluidTag());
     }
 }
