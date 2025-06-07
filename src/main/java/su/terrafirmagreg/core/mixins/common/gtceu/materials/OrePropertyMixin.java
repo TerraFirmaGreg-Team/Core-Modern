24dc7414fd061af4fe8c20231e7bc845b9203870
Pyritie <pyritie@gmail.com>
2025-06-07 21:41:30 +0100

    fixed gregtech's setOreByProducts so it actually clears the list first

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/materials/OrePropertyMixin.java
@@ -0,0 +1,25 @@
+package su.terrafirmagreg.core.mixins.common.gtceu.materials;
+
+import com.gregtechceu.gtceu.api.data.chemical.material.Material;
+import com.gregtechceu.gtceu.api.data.chemical.material.properties.OreProperty;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(value = OreProperty.class, remap = false)
+public abstract class OrePropertyMixin {
+
+	@Shadow @Final private List<Material> oreByProducts;
+
+	// Clear the byproduct list before adding new ones, otherwise it can't be edited in kjs
+	@Inject(method = "setOreByProducts", at = @At("HEAD"), remap = false)
+	public void tfg$setOreByProducts(Material[] materials, CallbackInfo ci)
+	{
+		this.oreByProducts.clear();
+	}
+}
