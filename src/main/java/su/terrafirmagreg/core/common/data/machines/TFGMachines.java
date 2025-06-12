d4d31fb3b95cac1f8e2b974dc8020fa673aa9017
Pyritie <pyritie@gmail.com>
2025-06-13 00:40:06 +0100

    added the little tree back to the multiblock preview, though it crashes if you click on anything :(

--- src/main/java/su/terrafirmagreg/core/common/data/machines/TFGMachines.java
+++ src/main/java/su/terrafirmagreg/core/common/data/machines/TFGMachines.java
@@ -1,6 +1,6 @@
 package su.terrafirmagreg.core.common.data.machines;
 
-import com.alekiponi.firmaciv.util.TFCWood;
+import appeng.core.definitions.AEBlocks;
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.data.RotationState;
@@ -12,39 +12,30 @@ import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.SimpleTieredMachine;
 import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
-import com.gregtechceu.gtceu.api.machine.steam.SimpleSteamMachine;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.MultiblockShapeInfo;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
 import com.gregtechceu.gtceu.api.pattern.TraceabilityPredicate;
-import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
-import com.gregtechceu.gtceu.api.recipe.modifier.RecipeModifier;
 import com.gregtechceu.gtceu.api.registry.registrate.MachineBuilder;
-import com.gregtechceu.gtceu.client.renderer.machine.WorkableSteamMachineRenderer;
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTMachines;
-import com.gregtechceu.gtceu.common.data.GTMaterialBlocks;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
-import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
-import com.gregtechceu.gtceu.common.data.machines.GTAEMachines;
 import com.gregtechceu.gtceu.common.data.machines.GTMachineUtils;
-import com.gregtechceu.gtceu.common.data.machines.GTMultiMachines;
-import com.gregtechceu.gtceu.common.registry.GTRegistration;
-import com.gregtechceu.gtceu.utils.FormattingUtil;
 
-import net.dries007.tfc.common.TFCTags;
 import net.dries007.tfc.common.blocks.TFCBlocks;
 import net.dries007.tfc.common.blocks.soil.SoilBlockType;
 import net.dries007.tfc.common.blocks.wood.Wood;
-import net.dries007.tfc.common.recipes.ingredients.BlockIngredient.BlockTag;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.AirBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.BiFunction;
 
 import static su.terrafirmagreg.core.TFGCore.REGISTRATE;
@@ -68,16 +59,9 @@ public class TFGMachines {
 		.rotationState(RotationState.NON_Y_AXIS)
 		.recipeType(TFGRecipeTypes.GREENHOUSE_RECIPES)
 		.recipeModifier(GTRecipeModifiers.OC_PERFECT)
-		.appearanceBlock(GTBlocks.CASING_STEEL_SOLID)
+		.appearanceBlock(GTBlocks.STEEL_HULL)
 		.workableCasingRenderer(GTCEu.id("block/casings/solid/machine_casing_solid_steel"), GTCEu.id("block/multiblock/implosion_compressor"), false)
-		.pattern(definition -> {
-			TraceabilityPredicate energyHatch = Predicates.abilities(PartAbility.INPUT_ENERGY).setMinGlobalLimited(1).setMaxGlobalLimited(2);
-			TraceabilityPredicate importBus = Predicates.abilities(PartAbility.IMPORT_ITEMS).setExactLimit(1);
-			TraceabilityPredicate exportBus = Predicates.abilities(PartAbility.EXPORT_ITEMS).setExactLimit(1);
-			TraceabilityPredicate importHatch = Predicates.abilities(PartAbility.IMPORT_FLUIDS).setExactLimit(1);
-			TraceabilityPredicate maintHatch = Predicates.autoAbilities(true, false, false).setExactLimit(1);
-
-			return FactoryBlockPattern.start()
+		.pattern(definition -> FactoryBlockPattern.start()
 			.aisle("CCCCCCC", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "   F   ")
 			.aisle("CDDDDDC", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", " XXFXX ")
 			.aisle("CDDDDDC", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", " XXFXX ")
@@ -85,14 +69,43 @@ public class TFGMachines {
 			.aisle("CDDDDDC", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", " XXFXX ")
 			.aisle("CDDDDDC", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", " XXFXX ")
 			.aisle("CCCYCCC", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "   F   ")
-			.where('C', Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get()).or(importBus).or(exportBus).or(importHatch).or(maintHatch).or(energyHatch))
 			.where('Y', Predicates.controller(Predicates.blocks(definition.get())))
-			.where('#', Predicates.air())
+			.where('C', Predicates.blocks(GTBlocks.STEEL_HULL.get()).setMinGlobalLimited(15)
+				.or(Predicates.autoAbilities(definition.getRecipeTypes()))
+				.or(Predicates.autoAbilities(true, false, false)))
+			.where('#', Predicates.air()
+				.or(Predicates.blockTag(BlockTags.LOGS))
+				.or(Predicates.blockTag(BlockTags.LEAVES)))
 			.where(' ', Predicates.any())
 			.where('F', Predicates.frames(GTMaterials.Steel))
 			.where('X', Predicates.blocks(ForgeRegistries.BLOCKS.getValue(new ResourceLocation("ae2", "quartz_glass"))))
 			.where('D', Predicates.blockTag(BlockTags.DIRT))
-			.build();
+			.build())
+		.shapeInfos(definition -> {
+			List<MultiblockShapeInfo> shapeInfo = new ArrayList<>();
+			var builder = MultiblockShapeInfo.builder()
+				.aisle("CCCCCCC", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "   F   ")
+				.aisle("CDDDDDC", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", " XXFXX ")
+				.aisle("CDDDDDC", "X#####X", "X#####X", "X#####X", "X##L##X", "X#LLL#X", "X##L##X", "X#####X", " XXFXX ")
+				.aisle("CDDDDDC", "F##W##F", "F##W##F", "F##W##F", "F#LWL#F", "F#LWL#F", "F#LLL#F", "F#####F", "FFFFFFF")
+				.aisle("CDDDDDC", "X#####X", "X#####X", "X#####X", "X##L##X", "X#LLL#X", "X##L##X", "X#####X", " XXFXX ")
+				.aisle("CDDDDDC", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", "X#####X", " XXFXX ")
+				.aisle("mitYfeC", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "XXXFXXX", "   F   ")
+				.where('Y', definition, Direction.SOUTH)
+				.where('C', GTBlocks.STEEL_HULL.getDefaultState())
+				.where('D', TFCBlocks.SOIL.get(SoilBlockType.GRASS).get(SoilBlockType.Variant.LOAM).get())
+				.where('F', ChemicalHelper.getBlock(TagPrefix.frameGt, GTMaterials.Steel))
+				.where('X', ForgeRegistries.BLOCKS.getValue(new ResourceLocation("ae2", "quartz_glass")))
+				.where('W', TFCBlocks.WOODS.get(Wood.OAK).get(Wood.BlockType.LOG).get())
+				.where('L', TFCBlocks.WOODS.get(Wood.OAK).get(Wood.BlockType.LEAVES).get())
+				.where(' ', Blocks.AIR)
+				.where('i', GTMachines.ITEM_IMPORT_BUS[GTValues.ULV], Direction.SOUTH)
+				.where('t', GTMachines.ITEM_EXPORT_BUS[GTValues.MV], Direction.SOUTH)
+				.where('f', GTMachines.FLUID_IMPORT_HATCH[GTValues.ULV], Direction.SOUTH)
+				.where('e', GTMachines.ENERGY_INPUT_HATCH[GTValues.LV], Direction.SOUTH)
+				.where('m', GTMachines.MAINTENANCE_HATCH, Direction.SOUTH);
+			shapeInfo.add(builder.build());
+			return shapeInfo;
 		})
 		.register();
 
