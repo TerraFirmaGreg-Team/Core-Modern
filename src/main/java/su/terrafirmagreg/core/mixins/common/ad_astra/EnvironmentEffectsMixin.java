bbb44e64c24d787eea684b60ec664e1e76819549
Pyritie <pyritie@gmail.com>
2025-07-01 23:56:57 +0100

    Better way to prevent lunar blast furnaces and bloomeries, also added sound effects to other things extinguishing

--- src/main/java/su/terrafirmagreg/core/mixins/common/ad_astra/EnvironmentEffectsMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/ad_astra/EnvironmentEffectsMixin.java
@@ -9,10 +9,13 @@ import net.dries007.tfc.common.blocks.*;
 import net.dries007.tfc.common.blocks.devices.*;
 import net.dries007.tfc.common.blocks.soil.FarmlandBlock;
 import net.dries007.tfc.common.blocks.soil.IGrassBlock;
+import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.LampFuel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.WallTorchBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -47,6 +50,8 @@ public abstract class EnvironmentEffectsMixin {
 		tfg$tickBlockBugWorkaround(level, pos, state);
 	}
 
+	// Bloomery and Blast furnace can't be extinguished after being started (outside of like, breaking them),
+	// so they get their own mixins
 	@Unique
 	private static void tfg$tickBlockBugWorkaround(ServerLevel level, BlockPos pos, BlockState state)
 	{
@@ -55,56 +60,59 @@ public abstract class EnvironmentEffectsMixin {
 			return;
 
 		if (block instanceof TFCTorchBlock) {
+			Helpers.playSound(level, pos, SoundEvents.FIRE_EXTINGUISH);
 			level.setBlockAndUpdate(pos, TFCBlocks.DEAD_TORCH.get().defaultBlockState());
 		} else if (block instanceof TFCWallTorchBlock) {
-			level.setBlockAndUpdate(pos, TFCBlocks.DEAD_WALL_TORCH.get().defaultBlockState());
+			Helpers.playSound(level, pos, SoundEvents.FIRE_EXTINGUISH);
+			level.setBlockAndUpdate(pos, Helpers.copyProperty(TFCBlocks.DEAD_WALL_TORCH.get().defaultBlockState(), state, WallTorchBlock.FACING));
 		} else if (block instanceof IGrassBlock grassBlock) {
 			level.setBlockAndUpdate(pos, grassBlock.getDirt());
 		} else if (block instanceof FarmlandBlock) {
 			FarmlandBlock.turnToDirt(state, level, pos);
 		} else if (block instanceof TFCCandleBlock) {
-			level.setBlockAndUpdate(pos, state.setValue(TFCCandleBlock.LIT, false));
+			if (state.getValue(TFCCandleBlock.LIT)) {
+				Helpers.playSound(level, pos, SoundEvents.FIRE_EXTINGUISH);
+				level.setBlockAndUpdate(pos, state.setValue(TFCCandleBlock.LIT, false));
+			}
 		} else if (block instanceof FirepitBlock) {
-			var be = level.getBlockEntity(pos);
-			if (be instanceof AbstractFirepitBlockEntity<?> firepit) {
-				firepit.extinguish(state);
+			if (state.getValue(FirepitBlock.LIT)) {
+				var be = level.getBlockEntity(pos);
+				if (be instanceof AbstractFirepitBlockEntity<?> firepit) {
+					firepit.extinguish(state);
+				}
 			}
 		} else if (block instanceof TFCCandleCakeBlock) {
-			level.setBlockAndUpdate(pos, state.setValue(TFCCandleCakeBlock.LIT, false));
+			if (state.getValue(TFCCandleCakeBlock.LIT)) {
+				Helpers.playSound(level, pos, SoundEvents.FIRE_EXTINGUISH);
+				level.setBlockAndUpdate(pos, state.setValue(TFCCandleCakeBlock.LIT, false));
+			}
 		} else if (block instanceof PitKilnBlock) {
+			Helpers.playSound(level, pos, SoundEvents.FIRE_EXTINGUISH);
 			level.destroyBlock(pos, false);
-		} else if (block instanceof BloomeryBlock) {
-			// TODO: doesn't seem to work
-			var be = level.getBlockEntity(pos, TFCBlockEntities.BLOOMERY.get());
-			if (be.isPresent()) {
-				be.get().ejectInventory();
-			}
-			level.setBlockAndUpdate(pos, state.setValue(BloomeryBlock.LIT, false));
-			level.setBlockAndUpdate(pos, state.setValue(BloomeryBlock.OPEN, true));
-		} else if (block instanceof BlastFurnaceBlock) {
-			// TODO: doesn't seem to work
-			var be = level.getBlockEntity(pos, TFCBlockEntities.BLAST_FURNACE.get());
-			if (be.isPresent()) {
-				be.get().extinguish(state);
-			}
 		} else if (block instanceof OvenBottomBlock) {
-			var be = level.getBlockEntity(pos, FLBlockEntities.OVEN_BOTTOM.get());
-			if (be.isPresent()) {
-				be.get().extinguish(state);
+			if (state.getValue(OvenBottomBlock.LIT)) {
+				var be = level.getBlockEntity(pos, FLBlockEntities.OVEN_BOTTOM.get());
+				if (be.isPresent()) {
+					be.get().extinguish(state);
+				}
 			}
 		} else if (block instanceof CharcoalForgeBlock) {
+			Helpers.playSound(level, pos, SoundEvents.FIRE_EXTINGUISH);
 			level.setBlockAndUpdate(pos, TFCBlocks.CHARCOAL_PILE.get().defaultBlockState().setValue(CharcoalPileBlock.LAYERS, 7));
 		} else if (block instanceof JackOLanternBlock jackOLantern) {
 			jackOLantern.extinguish(level, pos, state);
 		} else if (block instanceof LampBlock) {
-			var be = level.getBlockEntity(pos, TFCBlockEntities.LAMP.get());
-			if (be.isPresent()) {
-				LampFuel fuel = be.get().getFuel();
-				if (fuel != null && fuel.getBurnRate() < 0) {
-					return;
+			if (state.getValue(LampBlock.LIT)) {
+				var be = level.getBlockEntity(pos, TFCBlockEntities.LAMP.get());
+				if (be.isPresent()) {
+					LampFuel fuel = be.get().getFuel();
+					if (fuel != null && fuel.getBurnRate() < 0) {
+						return;
+					}
 				}
+				Helpers.playSound(level, pos, SoundEvents.FIRE_EXTINGUISH);
+				level.setBlockAndUpdate(pos, state.setValue(LampBlock.LIT, false));
 			}
-			level.setBlockAndUpdate(pos, state.setValue(LampBlock.LIT, false));
 		}
 	}
 }
