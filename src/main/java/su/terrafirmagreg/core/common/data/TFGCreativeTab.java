ffe406714eb3477b8364843c8af894c1799a1708
Redeix <brayden.j.m.ford@gmail.com>
2025-07-03 16:24:30 -0500

    - Added cardinal rotation to some block builders. And removed redundant classes.

--- src/main/java/su/terrafirmagreg/core/common/data/TFGCreativeTab.java
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGCreativeTab.java
@@ -32,10 +32,6 @@ public class TFGCreativeTab {
 	{
 		accept(out, TFGBlocks.LUNAR_CHORUS_PLANT);
 		accept(out, TFGBlocks.LUNAR_CHORUS_FLOWER);
-		accept(out, TFGBlocks.GEYSER_SOURCE);
-		accept(out, TFGBlocks.GEYSER_SOURCE_SMALL);
-		accept(out, TFGBlocks.STROMATOLITE_CLUSTER_MEDIUM);
-		accept(out, TFGBlocks.STROMATOLITE_CLUSTER_SMALL);
 
 		accept(out, TFGItems.PIGLIN_DISGUISE);
 		accept(out, TFGItems.TROWEL);
