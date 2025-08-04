8e94bce7c3a750393fa6db4a6b94114dfc0eb941
Pyritie <pyritie@gmail.com>
2025-08-05 00:25:28 +0100

    added crafting components, changed food oven recipe a little

--- src/main/java/su/terrafirmagreg/core/common/data/tfgt/TFGTRecipes.java
+++ src/main/java/su/terrafirmagreg/core/common/data/tfgt/TFGTRecipes.java
@@ -11,12 +11,15 @@ import com.gregtechceu.gtceu.data.recipe.CustomTags;
 import com.gregtechceu.gtceu.data.recipe.GTCraftingComponents;
 import com.gregtechceu.gtceu.data.recipe.VanillaRecipeHelper;
 import com.gregtechceu.gtceu.data.recipe.misc.MetaTileEntityLoader;
+import electrolyte.greate.foundation.data.recipe.GreateCraftingComponents;
+import net.dries007.tfc.common.blocks.TFCBlocks;
 import net.dries007.tfc.common.items.TFCItems;
 import net.minecraft.data.recipes.FinishedRecipe;
 import su.terrafirmagreg.core.TFGCore;
 import su.terrafirmagreg.core.common.data.TFGItems;
 import su.terrafirmagreg.core.common.data.tfgt.machine.TFGMachines;
 import su.terrafirmagreg.core.common.data.tfgt.machine.TFGMultiMachines;
+import su.terrafirmagreg.core.common.data.tfgt.machine.TFGTCraftingComponents;
 
 import java.util.function.Consumer;
 
@@ -27,9 +30,7 @@ public class TFGTRecipes {
 
 	public static void init(Consumer<FinishedRecipe> provider)
 	{
-		// The steam aqueous accumulator recipe is in KJS
-
-		MetaTileEntityLoader.registerMachineRecipe(provider, TFGMachines.AQUEOUS_ACCUMULATOR,
+		MetaTileEntityLoader.registerMachineRecipe(provider, true, TFGMachines.AQUEOUS_ACCUMULATOR,
 			"RPR", "CHC", "GGG",
 			'P', GTCraftingComponents.PUMP,
 			'R', GTCraftingComponents.ROTOR,
@@ -37,7 +38,7 @@ public class TFGTRecipes {
 			'H', GTCraftingComponents.HULL,
 			'G', GTCraftingComponents.GLASS);
 
-		MetaTileEntityLoader.registerMachineRecipe(provider, TFGMachines.FOOD_OVEN,
+		MetaTileEntityLoader.registerMachineRecipe(provider, true, TFGMachines.FOOD_OVEN,
 			"DTD", "AHB", "COC",
 			'T', FLBlocks.CURED_OVEN_TOP.get(OvenType.BRICK).get(),
 			'H', GTCraftingComponents.HULL,
@@ -45,10 +46,9 @@ public class TFGTRecipes {
 			'B', GTCraftingComponents.CABLE,
 			'C', GTCraftingComponents.COIL_HEATING_DOUBLE,
 			'D', GTCraftingComponents.PLATE,
-			// This is replaced with #tfg:metal_bars in kubejs
-			'O', GTCraftingComponents.PISTON);
+			'O', TFCItems.WROUGHT_IRON_GRILL.get());
 
-		MetaTileEntityLoader.registerMachineRecipe(provider, TFGMachines.FOOD_PROCESSOR,
+		MetaTileEntityLoader.registerMachineRecipe(provider, true, TFGMachines.FOOD_PROCESSOR,
 			"BGC", "MHW", "AVP",
 			'H', GTCraftingComponents.HULL,
 			'B', GTCraftingComponents.CABLE,
@@ -58,20 +58,18 @@ public class TFGTRecipes {
 			'P', GTCraftingComponents.PUMP,
 			'G', GTCraftingComponents.GLASS,
 			'C', GTCraftingComponents.CIRCUIT,
-			// This is replaced with Greate's Whisk in kubejs
-			'W', GTCraftingComponents.PISTON);
+			'W', TFGTCraftingComponents.WHISK);
 
-		MetaTileEntityLoader.registerMachineRecipe(provider, TFGMachines.FOOD_REFRIGERATOR,
+		MetaTileEntityLoader.registerMachineRecipe(provider, true, TFGMachines.FOOD_REFRIGERATOR,
 			"CFC", "SHS", "PRP",
 			'C', GTCraftingComponents.CABLE,
 			'F', GTCraftingComponents.CIRCUIT,
 			'S', ChemicalHelper.get(TagPrefix.plate, GTMaterials.Polyethylene),
-			// This is replaced with Hermetic Casing in kubejs
-			'H', GTCraftingComponents.HULL,
+			'H', TFGTCraftingComponents.HERMETIC_CASING,
 			'P', GTCraftingComponents.PUMP,
 			'R', GTCraftingComponents.ROTOR);
 
-		MetaTileEntityLoader.registerMachineRecipe(provider, TFGMachines.GAS_PRESSURIZER,
+		MetaTileEntityLoader.registerMachineRecipe(provider, true, TFGMachines.GAS_PRESSURIZER,
 			"GIG", "RHC", "EPE",
 			'H', GTCraftingComponents.HULL,
 			'I', GTCraftingComponents.PISTON,
@@ -84,16 +82,20 @@ public class TFGTRecipes {
 
 		for (int i = 0; i < TFGMachines.RAILGUN_ITEM_LOADER_IN.length; i++) {
 			if (TFGMachines.RAILGUN_ITEM_LOADER_IN[i] != null && TFGMachines.RAILGUN_ITEM_LOADER_OUT[i] != null) {
-				VanillaRecipeHelper.addShapedRecipe(provider, TFGCore.id("railgun_input_bus_create_" + TFGMachines.RAILGUN_ITEM_LOADER_IN[i].getTier()),
+				VanillaRecipeHelper.addShapedRecipe(provider, true,
+					TFGCore.id("railgun_input_bus_create_" + TFGMachines.RAILGUN_ITEM_LOADER_IN[i].getTier()),
 						TFGMachines.RAILGUN_ITEM_LOADER_IN[i].asStack(),
 						" d ", "rBx", " w ",
 						'B', ITEM_IMPORT_BUS[i].asStack());
 
-				VanillaRecipeHelper.addShapedRecipe(provider, TFGCore.id("railgun_input_convert_" + TFGMachines.RAILGUN_ITEM_LOADER_IN[i].getTier()),
+				VanillaRecipeHelper.addShapedRecipe(provider, true,
+					TFGCore.id("railgun_input_convert_" + TFGMachines.RAILGUN_ITEM_LOADER_IN[i].getTier()),
 						TFGMachines.RAILGUN_ITEM_LOADER_IN[i].asStack(),
 						"d", "B",
 						'B', TFGMachines.RAILGUN_ITEM_LOADER_OUT[i].asStack());
-				VanillaRecipeHelper.addShapedRecipe(provider, TFGCore.id("railgun_output_convert_" + TFGMachines.RAILGUN_ITEM_LOADER_OUT[i].getTier()),
+
+				VanillaRecipeHelper.addShapedRecipe(provider, true,
+					TFGCore.id("railgun_output_convert_" + TFGMachines.RAILGUN_ITEM_LOADER_OUT[i].getTier()),
 						TFGMachines.RAILGUN_ITEM_LOADER_OUT[i].asStack(),
 						"d", "B",
 						'B', TFGMachines.RAILGUN_ITEM_LOADER_IN[i].asStack());
