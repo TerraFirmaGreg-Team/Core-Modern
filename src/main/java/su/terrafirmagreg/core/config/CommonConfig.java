4fdcccc82e3e0200f37718616eac6bac17587bc8
Zippity <i.wa.anderson.86@gmail.com>
2025-08-17 15:41:54 -0700

    futureproof and document

--- src/main/java/su/terrafirmagreg/core/config/CommonConfig.java
+++ src/main/java/su/terrafirmagreg/core/config/CommonConfig.java
@@ -2,23 +2,18 @@ package su.terrafirmagreg.core.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+/**
+ * Common Config
+ * Not synced with server, saved per Minecraft instance
+ * Use for config settings that don't make sense to have per-world.
+ */
 public final class CommonConfig {
     public final ForgeConfigSpec.BooleanValue ENABLE_TFC_AMBIENTAL_COMPAT;
     public final ForgeConfigSpec.BooleanValue ENABLE_CREATE_COMPAT;
-
-    /**
-     * @deprecated (in CommonConfig) as of {@code 0.7.11}. This setting is clientside and should be moved to a new ClientConfig file.
-     */
-    @Deprecated
-    public final ForgeConfigSpec.IntValue PRECISE_ORE_PROSPECTOR_PARTICLE_CHANCE;
 
     CommonConfig(ForgeConfigSpec.Builder builder) {
         builder.push("general");
         ENABLE_CREATE_COMPAT = builder.comment("Should be create compat enabled?").define("createCompat", true);
         ENABLE_TFC_AMBIENTAL_COMPAT = builder.comment("Should be tfc ambiental compat enabled?").define("tfcAmbientalCompat", true);
-        PRECISE_ORE_PROSPECTOR_PARTICLE_CHANCE = builder
-                .comment("\n\n1 in N chance for the precise xray ore prospector particles to appear per block. Set to 0 to disable. Default: 5")
-                .defineInRange("PreciseOreProspectorParticleChance", 5, 0, 1000);
-        builder.pop();
     }
 }
