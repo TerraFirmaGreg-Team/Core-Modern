d9cdaab7fc10199f9e7ee4b02bb84e4ff7cde234
Pyritie <pyritie@gmail.com>
2025-09-09 14:07:10 +0100

    adds the vanilla advancements back since otherwise patchouli doesn't work

--- src/main/java/su/terrafirmagreg/core/mixins/common/minecraft/ServerAdvancementManagerMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/minecraft/ServerAdvancementManagerMixin.java
@@ -17,13 +17,17 @@ public abstract class ServerAdvancementManagerMixin {
      * Препятствует добавлению достижений в игру.
      * Но не удаляет достижения ваниллы, так как из-за этого перестает работать книга Patchouli.
      * Стоит поискать решение.
+     * -
+     * Prevents achievements from other mods.
+     * However, it doesn't remove vanilla achievements, because otherwise Patchouli stops working.
+     * It's worth looking for a better solution to hide the vanilla advancements.
      * */
     @Redirect(
             method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/AdvancementList;add(Ljava/util/Map;)V")
     )
     private void tfg$apply$advancementList$add(AdvancementList list, Map<ResourceLocation, Advancement.Builder> map) {
-        map.entrySet().removeIf(entry -> !entry.getKey().getNamespace().equals("gtceu"));
+        map.entrySet().removeIf(entry -> !entry.getKey().getNamespace().equals("minecraft"));
         list.add(map);
     }
 }
