2e8c7608922a83b28d5e2e663be49d9177b16e11
Kolja <104643407+Thatguyello@users.noreply.github.com>
2025-09-10 01:34:23 +0200

    spotless...

--- src/main/java/su/terrafirmagreg/core/common/TFGInteractionManager.java
+++ src/main/java/su/terrafirmagreg/core/common/TFGInteractionManager.java
@@ -1,25 +1,9 @@
 package su.terrafirmagreg.core.common;
 
-import net.dries007.tfc.common.items.Powder;
-import net.dries007.tfc.common.items.TFCItems;
-import net.dries007.tfc.util.Helpers;
-import net.dries007.tfc.util.InteractionManager;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import su.terrafirmagreg.core.common.data.TFGBlocks;
-import su.terrafirmagreg.core.common.data.blocks.LayerBlock;
 
 public class TFGInteractionManager {
-	public static void init(FMLCommonSetupEvent event)
-	{
+    public static void init(FMLCommonSetupEvent event) {
 //		InteractionManager.register(Ingredient.of(TFCItems.POWDERS.get(Powder.WOOD_ASH).get()), false, (stack, context) -> {
 //			Player player = context.getPlayer();
 //			if (player != null && !player.getAbilities().mayBuild)
@@ -64,5 +48,5 @@ public class TFGInteractionManager {
 //				return InteractionResult.FAIL;
 //			}
 //		});
-	}
+    }
 }
--- src/main/java/su/terrafirmagreg/core/mixins/common/create/ChainConveyorBlockEntityMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/create/ChainConveyorBlockEntityMixin.java
@@ -1,5 +1,16 @@
 package su.terrafirmagreg.core.mixins.common.create;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
@@ -7,6 +18,7 @@ import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.simibubi.create.api.contraption.transformable.TransformableBlockEntity;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
 import com.simibubi.create.content.kinetics.chainConveyor.ChainConveyorBlockEntity;
+
 import net.createmod.catnip.nbt.NBTHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
@@ -18,55 +30,50 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import su.terrafirmagreg.core.compat.create.ChainGTMaterialInterface;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Mixin(value = ChainConveyorBlockEntity.class, remap = false)
-public abstract class ChainConveyorBlockEntityMixin extends KineticBlockEntity implements TransformableBlockEntity, ChainGTMaterialInterface {
+public abstract class ChainConveyorBlockEntityMixin extends KineticBlockEntity
+        implements TransformableBlockEntity, ChainGTMaterialInterface {
     @Unique
     public Map<BlockPos, Material> tfg$connectionMaterialStats = new HashMap<>();
 
-    @Shadow BlockPos chainDestroyedEffectToSend;
+    @Shadow
+    BlockPos chainDestroyedEffectToSend;
 
     public ChainConveyorBlockEntityMixin(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
     }
 
-    @Override @Unique
+    @Override
+    @Unique
     public void addConnectionMaterial(BlockPos connection, Material chainMat) {
         BlockPos localTarget = connection.subtract(worldPosition);
         tfg$connectionMaterialStats.put(localTarget, chainMat);
     }
 
-    @Override @Unique
+    @Override
+    @Unique
     public Material getConnectionMaterial(BlockPos connection) {
         return tfg$connectionMaterialStats.get(connection);
     }
 
-    @Override @Unique
+    @Override
+    @Unique
     public Item getConnectionChainItem(BlockPos connection) {
         Material chainMat = tfg$connectionMaterialStats.get(connection);
         return ChemicalHelper.get(TagPrefix.get("chain"), (chainMat != null) ? chainMat : GTMaterials.Copper).getItem();
     }
 
     @Inject(method = "removeConnectionTo(Lnet/minecraft/core/BlockPos;)Z", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/kinetics/chainConveyor/ChainConveyorBlockEntity;notifyUpdate()V"), remap = false)
-    private void tfg$removeConnectionTo(BlockPos target, CallbackInfoReturnable<Boolean> cir)
-    {
+    private void tfg$removeConnectionTo(BlockPos target, CallbackInfoReturnable<Boolean> cir) {
         BlockPos localTarget = target.subtract(worldPosition);
         tfg$connectionMaterialStats.remove(localTarget);
     }
+
     @Inject(method = "write(Lnet/minecraft/nbt/CompoundTag;Z)V", at = @At("TAIL"), remap = false)
-    private void tfg$write(CompoundTag compound, boolean clientPacket, CallbackInfo ci)
-    {
+    private void tfg$write(CompoundTag compound, boolean clientPacket, CallbackInfo ci) {
         compound.put("ChainMaterials", NBTHelper.writeCompoundList(tfg$connectionMaterialStats.entrySet(), entry -> {
             CompoundTag compoundTag = new CompoundTag();
             compoundTag.put("Target", NbtUtils.writeBlockPos(entry.getKey()));
@@ -74,14 +81,14 @@ public abstract class ChainConveyorBlockEntityMixin extends KineticBlockEntity i
             return compoundTag;
         }));
     }
-    @Inject(method = "read(Lnet/minecraft/nbt/CompoundTag;Z)V", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/kinetics/chainConveyor/ChainConveyorBlockEntity;updateBoxWorldPositions()V"),remap = false)
-    private void tfg$read(CompoundTag compound, boolean clientPacket, CallbackInfo ci)
-    {
+
+    @Inject(method = "read(Lnet/minecraft/nbt/CompoundTag;Z)V", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/kinetics/chainConveyor/ChainConveyorBlockEntity;updateBoxWorldPositions()V"), remap = false)
+    private void tfg$read(CompoundTag compound, boolean clientPacket, CallbackInfo ci) {
         tfg$connectionMaterialStats.clear();
         NBTHelper.iterateCompoundList(compound.getList("ChainMaterials", Tag.TAG_COMPOUND),
                 c -> tfg$connectionMaterialStats.put(NbtUtils.readBlockPos(c.getCompound("Target")),
                         GTMaterials.get(c.getString("Material"))));
-        //Debug print
+        // Debug print
 //        for (BlockPos pos : tfg$connectionMaterialStats.keySet())
 //        {
 //            String matName = tfg$connectionMaterialStats.get(pos).getName();
@@ -97,8 +104,7 @@ public abstract class ChainConveyorBlockEntityMixin extends KineticBlockEntity i
             chainDestroyedEffectToSend = target;
             sendData();
         }
-        if (!spawnDrops)
-        {
+        if (!spawnDrops) {
             ci.cancel();
             return;
         }
