73189d91c03c271cbf2ce344e2bfbdbead9e161b
TomPlop <tomdidome@gmail.com>
2025-09-29 12:09:03 -0400

    Finally makes it work

--- src/main/java/su/terrafirmagreg/core/common/data/tfgt/TFGFissionComponents.java
+++ src/main/java/su/terrafirmagreg/core/common/data/tfgt/TFGFissionComponents.java
@@ -2,6 +2,7 @@ package su.terrafirmagreg.core.common.data.tfgt;
 
 import net.minecraft.world.level.block.Blocks;
 
+import earth.terrarium.adastra.common.registry.ModBlocks;
 import fi.dea.mc.deafission.common.data.FissionComponents;
 import fi.dea.mc.deafission.core.components.EfficiencyComponent;
 import fi.dea.mc.deafission.core.components.HeatComponent;
@@ -12,11 +13,8 @@ public class TFGFissionComponents {
 
         FissionComponents.heat.put(Blocks.BEDROCK, new HeatComponent(Blocks.BEDROCK, 15_000));
         FissionComponents.heat.put(Blocks.BLUE_ICE, new HeatComponent(Blocks.BLUE_ICE, 0.5));
-        /*
-        Block glacialFur = ForgeRegistries.BLOCKS.getValue(ResourceLocation.fromNamespaceAndPath("ad_astra", "glacial_fur"));
-        {
-            FissionComponents.heat.put(glacialFur, new HeatComponent(glacialFur, 2));
-        }*/
+
+        FissionComponents.heat.put(ModBlocks.GLACIAN_FUR.get(), new HeatComponent(ModBlocks.GLACIAN_FUR.get(), 2));
 
         FissionComponents.efficiency.put(Blocks.DIAMOND_BLOCK, new EfficiencyComponent(Blocks.DIAMOND_BLOCK, 10_000));
 
