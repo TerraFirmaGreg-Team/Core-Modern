55c1de177d6ecea0a70f1861c2802f72d0f914d7
TomPlop <tomdidome@gmail.com>
2025-10-03 18:44:34 -0400

    Adding new components to the fission reactor

--- src/main/java/su/terrafirmagreg/core/common/data/tfgt/TFGFissionComponents.java
+++ src/main/java/su/terrafirmagreg/core/common/data/tfgt/TFGFissionComponents.java
@@ -18,15 +18,19 @@ public class TFGFissionComponents {
             .getValue(ResourceLocation.fromNamespaceAndPath("tfg", "glacian_wool_frame"));
     private static final Supplier<Block> aes_frame = () -> ForgeRegistries.BLOCKS
             .getValue(ResourceLocation.fromNamespaceAndPath("tfg", "aes_insulation_frame"));
+    private static final Supplier<Block> moderate_frame = () -> ForgeRegistries.BLOCKS
+            .getValue(ResourceLocation.fromNamespaceAndPath("tfg", "moderate_core_frame"));
+    private static final Supplier<Block> impure_moderate_frame = () -> ForgeRegistries.BLOCKS
+            .getValue(ResourceLocation.fromNamespaceAndPath("tfg", "impure_moderate_core_frame"));
 
     public static void addComponents() {
 
         FissionComponents.heat.put(Blocks.BEDROCK, new HeatComponent(Blocks.BEDROCK, 15_000));
         FissionComponents.heat.put(Blocks.BLUE_ICE, new HeatComponent(Blocks.BLUE_ICE, 0.5));
-
         FissionComponents.heat.put(glacian_frame.get(), new HeatComponent(glacian_frame.get(), 2));
-
         FissionComponents.heat.put(aes_frame.get(), new HeatComponent(aes_frame.get(), 1));
+        FissionComponents.heat.put(moderate_frame.get(), new HeatComponent(moderate_frame.get(), 10));
+        FissionComponents.heat.put(impure_moderate_frame.get(), new HeatComponent(impure_moderate_frame.get(), 5));
 
         FissionComponents.efficiency.put(Blocks.DIAMOND_BLOCK, new EfficiencyComponent(Blocks.DIAMOND_BLOCK, 10_000));
 
