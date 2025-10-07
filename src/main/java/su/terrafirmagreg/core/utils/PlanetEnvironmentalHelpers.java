fbd485e408549a0add46a8129f295929f828d39a
Pyritie <pyritie@gmail.com>
2025-10-07 18:19:08 +0100

    merging

---
+++ src/main/java/su/terrafirmagreg/core/utils/PlanetEnvironmentalHelpers.java
@@ -0,0 +1,82 @@
+package su.terrafirmagreg.core.utils;
+
+import static su.terrafirmagreg.core.TFGCore.LOGGER;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.Block;
+
+import lombok.Getter;
+
+import su.terrafirmagreg.core.common.data.TFGBlocks;
+import su.terrafirmagreg.core.common.data.TFGTags;
+
+public class PlanetEnvironmentalHelpers {
+    private static final List<Pair<TagKey<Biome>, MarsSandBlockType>> marsBiomeTags = Arrays.asList(
+            new Pair<>(TFGTags.Biomes.HasDarkSandWind, MarsSandBlockType.DEEP),
+            new Pair<>(TFGTags.Biomes.HasMediumSandWind, MarsSandBlockType.MEDIUM),
+            new Pair<>(TFGTags.Biomes.HasLightSandWind, MarsSandBlockType.LIGHT));
+
+    private static final Set<ResourceKey<Biome>> biomesWithoutTags = new HashSet<>();
+
+    /**
+     * Retrieves the correct sand layer block for a given block position.
+     *
+     * @param isPileBlock whether the layer block to be placed is a pile
+     */
+    public static Block getSandBlockForBiome(
+            LevelReader level, BlockPos pos, boolean isPileBlock) {
+        final Holder<Biome> currentBiome = level.getBiome(pos);
+
+        Block layer = marsBiomeTags.stream()
+                .findFirst()
+                .filter(pair -> currentBiome.is(pair.getFirst()))
+                .map(
+                        pair -> isPileBlock
+                                ? pair.getSecond().getPileBlock()
+                                : pair.getSecond().getLayerBlock())
+                .orElse(null);
+
+        if (layer == null) {
+            layer = isPileBlock
+                    ? MarsSandBlockType.MEDIUM.getPileBlock()
+                    : MarsSandBlockType.MEDIUM.getLayerBlock();
+            ResourceKey<Biome> biomeTag = currentBiome.unwrapKey().orElseThrow();
+            if (!biomesWithoutTags.contains(biomeTag)) {
+                biomesWithoutTags.add(biomeTag);
+                LOGGER.warn("{} is missing a sand wind biome tag! falling back to medium sand wind", biomeTag.location());
+            }
+        }
+
+        return layer;
+    }
+
+    public enum MarsSandBlockType {
+        DEEP(TFGBlocks.HEMATITIC_SAND_LAYER_BLOCK.get(), TFGBlocks.HEMATITIC_SAND_PILE_BLOCK.get()),
+        MEDIUM(TFGBlocks.MARS_SAND_LAYER_BLOCK.get(), TFGBlocks.MARS_SAND_PILE_BLOCK.get()),
+        LIGHT(TFGBlocks.VENUS_SAND_LAYER_BLOCK.get(), TFGBlocks.VENUS_SAND_PILE_BLOCK.get());
+
+        private static final MarsSandBlockType[] VALUES = values();
+
+        @Getter
+        private final Block layerBlock;
+        @Getter
+        private final Block pileBlock;
+
+        MarsSandBlockType(Block layerBlock, Block pileBlock) {
+            this.layerBlock = layerBlock;
+            this.pileBlock = pileBlock;
+        }
+    }
+}
