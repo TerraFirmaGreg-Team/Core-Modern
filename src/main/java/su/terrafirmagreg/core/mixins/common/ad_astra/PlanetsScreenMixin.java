31fba8274ded14171cec9206b25702d3818c8034
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2025-10-24 22:58:07 -0500

    more stuff that works?

--- src/main/java/su/terrafirmagreg/core/mixins/common/ad_astra/PlanetsScreenMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/ad_astra/PlanetsScreenMixin.java
@@ -2,6 +2,7 @@ package su.terrafirmagreg.core.mixins.common.ad_astra;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
@@ -23,7 +24,23 @@ import su.terrafirmagreg.core.TFGCore;
 @Mixin(PlanetsScreen.class)
 public class PlanetsScreenMixin {
     @Unique
-    private static final ResourceLocation JUPITER_TEXTURE = new ResourceLocation(TFGCore.MOD_ID, "textures/gui/jupiter_planet");
+    private static final ResourceLocation JUPITER_TEXTURE = ResourceLocation.fromNamespaceAndPath(TFGCore.MOD_ID, "textures/gui/jupiter_planet.png");
+    @Unique
+    private static final ResourceLocation MERCURY_BELT_TEXTURE = ResourceLocation.fromNamespaceAndPath(TFGCore.MOD_ID, "textures/gui/mercury_belt.png");
+    @Unique
+    private static final ResourceLocation ASTEROID_BELT_TEXTURE = ResourceLocation.fromNamespaceAndPath(TFGCore.MOD_ID, "textures/gui/asteroid_belt.png");
+    @Unique
+    private static final ResourceLocation MOON_ORBIT_TEXTURE = ResourceLocation.fromNamespaceAndPath(TFGCore.MOD_ID, "textures/gui/moon_orbit.png");
+
+    @ModifyArg(method = "lambda$static$11", at = @At(value = "INVOKE", target = "Learth/terrarium/adastra/client/screens/PlanetsScreen;drawCircles(IIILcom/mojang/blaze3d/vertex/BufferBuilder;II)V", ordinal = 0), index = 0, remap = false)
+    private static int tfg$modifySolarOrbits0(int value) {
+        return 1;
+    }
+
+    @ModifyArg(method = "lambda$static$11", at = @At(value = "INVOKE", target = "Learth/terrarium/adastra/client/screens/PlanetsScreen;drawCircles(IIILcom/mojang/blaze3d/vertex/BufferBuilder;II)V", ordinal = 0), index = 1, remap = false)
+    private static int tfg$modifySolarOrbits1(int value) {
+        return 3;
+    }
 
     static {
         AdAstraClientEvents.RenderSolarSystemEvent.register((graphics, solarSystem, width, height) -> {
@@ -33,15 +50,57 @@ public class PlanetsScreenMixin {
                 RenderSystem.setShader(GameRenderer::getPositionColorShader);
                 bufferBuilder.begin(VertexFormat.Mode.DEBUG_LINES, DefaultVertexFormat.POSITION_COLOR);
                 int color = 0xff24327b;
-                PlanetsScreen.drawCircle(bufferBuilder, (float) width / 2.0F, (float) height / 2.0F, 180, 75, color);
+                PlanetsScreen.drawCircle(bufferBuilder, (float) width / 2.0F, (float) height / 2.0F, 190, 75, color);
+                PlanetsScreen.drawCircle(bufferBuilder, (float) width / 2.0F, (float) height / 2.0F, 250, 75, color);
+
                 tessellator.end();
+
+                float rotation = (float) Util.getMillis() / 100.0F;
+
+                int mercury_belt_size = 72;
+                int mercury_belt_center = mercury_belt_size / 2;
+
+                // Mercury but a belt
+                graphics.pose().pushPose();
+                graphics.pose().translate(width / 2f - mercury_belt_center, height / 2f - mercury_belt_center, 0);
+                graphics.pose().rotateAround(Axis.ZP.rotationDegrees(rotation * 3 / 2), mercury_belt_center, mercury_belt_center, 0);
+                graphics.blit(MERCURY_BELT_TEXTURE, 0, 0, 0, 0, mercury_belt_size, mercury_belt_size, mercury_belt_size, mercury_belt_size);
+                graphics.pose().popPose();
+
+                int asteroid_belt_size = 342;
+                int asteroid_belt_center = asteroid_belt_size / 2;
+
+                // Asteroid Belt
+                graphics.pose().pushPose();
+                graphics.pose().translate(width / 2f - asteroid_belt_center, height / 2f - asteroid_belt_center, 0);
+                graphics.pose().rotateAround(Axis.ZP.rotationDegrees(rotation / 4 / 2), asteroid_belt_center, asteroid_belt_center, 0);
+                graphics.blit(ASTEROID_BELT_TEXTURE, 0, 0, 0, 0, asteroid_belt_size, asteroid_belt_size, asteroid_belt_size, asteroid_belt_size);
+                graphics.pose().popPose();
+
+                // Moon
+                graphics.pose().pushPose();
+                graphics.pose().translate((float) width / 2f, (float) height / 2f, 0);
+                graphics.pose().mulPose(Axis.ZP.rotationDegrees(rotation));
+                graphics.pose().translate(-8, -8, 0);
+                graphics.pose().translate((31 * 3 - 10), 0, 0);
+                graphics.pose().rotateAround(Axis.ZP.rotationDegrees(rotation * 4 / 2), 14, 14, 0);
+                graphics.blit(MOON_ORBIT_TEXTURE, 0, 0, 0, 0, 28, 28, 28, 28);
+                graphics.pose().popPose();
 
                 // Jupiter
                 graphics.pose().pushPose();
                 graphics.pose().translate(width / 2f, height / 2f, 0);
-                graphics.pose().mulPose(Axis.ZP.rotationDegrees((Util.getMillis() / 100f) / 5 / 2));
-                graphics.pose().translate(145, 0, 0);
-                graphics.blit(JUPITER_TEXTURE, 0, 0, 0, 0, 12, 12, 12, 12);
+                graphics.pose().mulPose(Axis.ZP.rotationDegrees(rotation / 5 / 2));
+                graphics.pose().translate(190 - 10, 0, 0);
+                graphics.blit(JUPITER_TEXTURE, 0, 0, 0, 0, 16, 16, 16, 16);
+                graphics.pose().popPose();
+
+                // Saturn
+                graphics.pose().pushPose();
+                graphics.pose().translate(width / 2f, height / 2f, 0);
+                graphics.pose().mulPose(Axis.ZP.rotationDegrees(rotation / 6 / 2));
+                graphics.pose().translate(250 - 10, 0, 0);
+                graphics.blit(JUPITER_TEXTURE, 0, 0, 0, 0, 16, 16, 16, 16);
                 graphics.pose().popPose();
 
             }
