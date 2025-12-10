5e60176bbb16d86c160b59bd0cf342c256b23eb4
Pyritie <pyritie@gmail.com>
2025-12-10 22:45:03 +0000

    fixes the mixin with gt hot ingots and tongs

--- src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/TagPrefixItemMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/TagPrefixItemMixin.java
@@ -6,27 +6,32 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.gregtechceu.gtceu.api.item.TagPrefixItem;
+import com.gregtechceu.gtceu.api.item.tool.ToolHelper;
 
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+
+import tfchotornot.common.HNTags;
 
 @Mixin(value = TagPrefixItem.class, remap = false)
 public abstract class TagPrefixItemMixin {
 
     /**
      * Если в руке горячий слиток, то если у вас в руках щипцы, то вы не будете получать урон.
+     * If you have a hot ingot in your hand, you will not take damage if you are holding tongs.
      */
     @Inject(method = "inventoryTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getItemBySlot(Lnet/minecraft/world/entity/EquipmentSlot;)Lnet/minecraft/world/item/ItemStack;", shift = At.Shift.AFTER), remap = true, cancellable = true)
-    private void tfg$inventoryTick$livingEntity$getItemBySlot(ItemStack stack, Level level, Entity entity, int slotId,
-            boolean isSelected, CallbackInfo ci) {
-        //        if (entity instanceof Player player) {
-        //            var stackInOffHand = player.getItemInHand(InteractionHand.OFF_HAND);
-        //            if (stackInOffHand.is(TFGTags.Items.Tongs)) {
-        //                ToolHelper.damageItem(stackInOffHand, player);
-        //                ci.cancel();
-        //            }
-        //        }
+    private void tfg$inventoryTick$livingEntity$getItemBySlot(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected, CallbackInfo ci) {
+        if (entity instanceof Player player) {
+            var stackInOffHand = player.getItemInHand(InteractionHand.OFF_HAND);
+            if (stackInOffHand.is(HNTags.Items.INSULATING)) {
+                ToolHelper.damageItem(stackInOffHand, player);
+                ci.cancel();
+            }
+        }
     }
 
 }
