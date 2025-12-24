c87b1e9ae2821f4467be8c92545bcdd2a3a56d3a
Mqrius <Nuntius.Marii@Gmail.com>
2025-12-24 23:36:41 +0100

    Prevent packed/blue ice melting to water source from hot items (#227)

--- src/main/java/su/terrafirmagreg/core/mixins/common/tfc/ForgeEventHandlerMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/tfc/ForgeEventHandlerMixin.java
@@ -5,7 +5,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
 import net.dries007.tfc.ForgeEventHandler;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.level.BlockEvent;
 
 @Mixin(value = ForgeEventHandler.class, remap = false)
@@ -18,5 +26,22 @@ public class ForgeEventHandlerMixin {
     private static void tfg$onCreateNetherPortal(BlockEvent.PortalSpawnEvent event, CallbackInfo ci) {
         event.setCanceled(true);
         ci.cancel();
+    }
+
+    // Don't create water source blocks when hot items melt the ice
+    // target = ServerLevel.setBlockAndUpdate(BlockPos, BlockState)
+    // There's a few of those in the method so have to check which blocks are being melted.
+    @WrapOperation(method = "onItemExpire", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;m_46597_(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z", remap = true), remap = false)
+    private static boolean tfg$preventHotIceWater(ServerLevel level, BlockPos pos, BlockState newState, Operation<Boolean> original) {
+        Block currentBlock = level.getBlockState(pos).getBlock();
+
+        // Only intercept packed/blue ice melting
+        if ((currentBlock == Blocks.PACKED_ICE || currentBlock == Blocks.BLUE_ICE) && newState.is(Blocks.WATER)) {
+            level.destroyBlock(pos, false);
+            return true;
+        }
+
+        // Otherwise do as usual
+        return original.call(level, pos, newState);
     }
 }
