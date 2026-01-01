02ec4ccabe7211ab69a383f936a8e3c0764dc4be
Pyritie <pyritie@gmail.com>
2026-01-01 22:56:47 +0000

    change attached decorative features to use lenient block states

--- src/main/java/su/terrafirmagreg/core/world/feature/AttachedDecorativePlantConfig.java
+++ src/main/java/su/terrafirmagreg/core/world/feature/AttachedDecorativePlantConfig.java
@@ -4,14 +4,14 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.dries007.tfc.world.Codecs;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 
-public record AttachedDecorativePlantConfig(Block block, int heightRange) implements FeatureConfiguration {
+public record AttachedDecorativePlantConfig(BlockState blockState, int heightRange) implements FeatureConfiguration {
 
     public static final Codec<AttachedDecorativePlantConfig> CODEC = RecordCodecBuilder.create(instance -> instance
             .group(
-                    Codecs.BLOCK.fieldOf("block").forGetter(c -> c.block),
+                    Codecs.BLOCK_STATE.fieldOf("block").forGetter(c -> c.blockState),
                     Codec.INT.fieldOf("heightRange").forGetter(c -> c.heightRange))
             .apply(instance, AttachedDecorativePlantConfig::new));
 }
