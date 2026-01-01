1d1baf75e05c93be3214944b1dae5832eada8a1f
Pyritie <pyritie@gmail.com>
2026-01-01 23:21:52 +0000

    troopers are now fireproof

--- src/main/java/su/terrafirmagreg/core/mixins/common/species/TrooperMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/species/TrooperMixin.java
@@ -11,15 +11,25 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 
 @Mixin(value = Trooper.class, remap = false)
-public class TrooperMixin {
+public abstract class TrooperMixin extends TamableAnimal {
+    public TrooperMixin(EntityType<? extends TamableAnimal> entityType, Level level) {
+        super(entityType, level);
+    }
 
     // Enable spawns
 
     @Inject(method = "canSpawn", at = @At("HEAD"), remap = false, cancellable = true)
     private static void tfg$canSpawn(EntityType<Trooper> entity, ServerLevelAccessor world, MobSpawnType spawnReason, BlockPos pos, RandomSource random, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(true);
+    }
+
+    @Override
+    public boolean fireImmune() {
+        return true;
     }
 }
