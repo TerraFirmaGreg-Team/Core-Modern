97e15c1dcd28e8c2994e7c56f7e0eccbb9dbbaaa
Pyritie <pyritie@gmail.com>
2026-01-02 02:39:21 +0000

    give blaze fireballs some physical damage, remove their noises

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/minecraft/BlazeMixin.java
@@ -0,0 +1,21 @@
+package su.terrafirmagreg.core.mixins.common.minecraft;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.monster.Blaze;
+
+@Mixin(value = Blaze.class)
+public class BlazeMixin {
+
+    // Change blaze's stupid loud annoying breathing noise
+
+    @Inject(method = "getAmbientSound", at = @At("HEAD"), cancellable = true)
+    protected void tfg$getAmbientSound(CallbackInfoReturnable<SoundEvent> cir) {
+        cir.setReturnValue(SoundEvents.FIRE_AMBIENT);
+    }
+}
