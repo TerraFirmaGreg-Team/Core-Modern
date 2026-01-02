cf69e8a34a715e7dd8bf343173b64bf8c1502c45
Pyritie <pyritie@gmail.com>
2026-01-02 16:32:28 +0000

    the bugs are now more aggressive

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/arthropocolypse/IceCrawlerMixin.java
@@ -0,0 +1,25 @@
+package su.terrafirmagreg.core.mixins.common.arthropocolypse;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.player.Player;
+
+@Pseudo
+@Mixin(targets = "dev.dh.arthropocolypse.entity.Ice_Crawler")
+public class IceCrawlerMixin {
+
+	// registerGoals
+    @SuppressWarnings("DataFlowIssue")
+    @Inject(method = "m_8099_", at = @At("TAIL"))
+    protected void tfg$registerGoals(CallbackInfo ci) {
+        PathfinderMob mob = (PathfinderMob) (Object) this;
+
+        mob.goalSelector.addGoal(2, new NearestAttackableTargetGoal<>(mob, Player.class, true, true));
+    }
+}
---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/arthropocolypse/SoldierAntMixin.java
@@ -0,0 +1,24 @@
+package su.terrafirmagreg.core.mixins.common.arthropocolypse;
+
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Pseudo
+@Mixin(targets = "dev.dh.arthropocolypse.entity.Soldier_Ant")
+public class SoldierAntMixin {
+
+	// registerGoals
+	@SuppressWarnings("DataFlowIssue")
+	@Inject(method = "m_8099_", at = @At("TAIL"))
+	protected void tfg$registerGoals(CallbackInfo ci) {
+		Animal mob = (Animal) (Object) this;
+
+		mob.goalSelector.addGoal(8, new NearestAttackableTargetGoal<>(mob, Player.class, true, true));
+	}
+}
