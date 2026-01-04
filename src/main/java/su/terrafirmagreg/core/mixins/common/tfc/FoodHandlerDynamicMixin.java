a8d57b1abd0739156db2f292136fc253d3c3c2e2
Mqrius <Nuntius.Marii@Gmail.com>
2026-01-04 01:54:40 +0100

    Fix ConcurrentModification, copy ingredients to sort before serializing.

--- src/main/java/su/terrafirmagreg/core/mixins/common/tfc/FoodHandlerDynamicMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/tfc/FoodHandlerDynamicMixin.java
@@ -1,26 +1,27 @@
 package su.terrafirmagreg.core.mixins.common.tfc;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import net.dries007.tfc.common.capabilities.food.FoodHandler;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 
 @Mixin(FoodHandler.Dynamic.class)
 public class FoodHandlerDynamicMixin {
 
     // Sort ingredients in dynamic foods before serializing. This ensures stacks with different ingredient order can stack.
-    @Inject(method = "serializeNBT", at = @At("HEAD"), remap = false)
-    private void sortIngredientsBeforeSerialize(CallbackInfoReturnable<CompoundTag> cir) {
-        List<ItemStack> ingredients = ((FoodHandler.Dynamic) (Object) this).getIngredients();
-        if (ingredients != null && ingredients.size() > 1) {
-            ingredients.sort(Comparator.comparing(ItemStack::toString));
+    @ModifyArg(method = "serializeNBT", at = @At(value = "INVOKE", target = "Lnet/dries007/tfc/util/Helpers;writeItemStacksToNbt(Ljava/util/List;)Lnet/minecraft/nbt/ListTag;"), remap = false)
+    private List<ItemStack> sortIngredientsBeforeSerialize(List<ItemStack> ingredients) {
+        if (ingredients == null || ingredients.size() <= 1) {
+            return ingredients;
         }
+        List<ItemStack> sorted = new ArrayList<>(ingredients);
+        sorted.sort(Comparator.comparing(ItemStack::toString));
+        return sorted;
     }
 }
