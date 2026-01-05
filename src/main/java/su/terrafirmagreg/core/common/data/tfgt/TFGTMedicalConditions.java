c189074a846e836fa7eeabe211810caf43727db3
Pyritie <pyritie@gmail.com>
2026-01-05 14:38:49 +0000

    made radioactive's effects trigger 10x as early

--- src/main/java/su/terrafirmagreg/core/common/data/tfgt/TFGTMedicalConditions.java
+++ src/main/java/su/terrafirmagreg/core/common/data/tfgt/TFGTMedicalConditions.java
@@ -8,7 +8,7 @@ public class TFGTMedicalConditions {
 
 	public static final Symptom GLOWING = new Symptom("symptom.tfg.glowing", 5, 1, MobEffects.GLOWING);
 
-    public static final MedicalCondition RADIOACTIVE = new MedicalCondition("radioactive", 0x00ff00, 20000,
+    public static final MedicalCondition RADIOACTIVE = new MedicalCondition("radioactive", 0x00ff00, 2000,
             MedicalCondition.IdleProgressionType.NONE, 0, true,
             new Symptom.ConfiguredSymptom(Symptom.DEATH),
             new Symptom.ConfiguredSymptom(Symptom.HEALTH_DEBUFF, 0.75f),
