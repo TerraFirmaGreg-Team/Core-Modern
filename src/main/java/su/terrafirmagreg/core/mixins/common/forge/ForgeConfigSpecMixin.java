cc90307cb92bd92fdc3ef07ae62c1d065f929791
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2026-01-23 19:51:01 -0600

    spotless

--- src/main/java/su/terrafirmagreg/core/mixins/common/forge/ForgeConfigSpecMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/forge/ForgeConfigSpecMixin.java
@@ -1,15 +1,17 @@
 package su.terrafirmagreg.core.mixins.common.forge;
 
-import net.minecraftforge.common.ForgeConfigSpec;
+import javax.annotation.CheckForNull;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import javax.annotation.CheckForNull;
+import net.minecraftforge.common.ForgeConfigSpec;
 
 /// This can be removed if TFC hot or not fixes its issues
 @Mixin(ForgeConfigSpec.ConfigValue.class)
 public class ForgeConfigSpecMixin {
     @Redirect(method = "get", at = @At(value = "INVOKE", target = "Lcom/google/common/base/Preconditions;checkState(ZLjava/lang/Object;)V"), remap = false)
-    public void tfg$cancelError(boolean expression, @CheckForNull Object errorMessage) {}
+    public void tfg$cancelError(boolean expression, @CheckForNull Object errorMessage) {
+    }
 }
