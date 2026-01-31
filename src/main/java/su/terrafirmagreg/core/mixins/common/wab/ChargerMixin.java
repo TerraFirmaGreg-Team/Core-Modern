19d74ec3a66c292b18cae0c8450a16eee2f492e6
Pyritie <pyritie@gmail.com>
2026-01-31 18:51:19 +0000

    things and stuff

--- src/main/java/su/terrafirmagreg/core/mixins/common/wab/ChargerMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/wab/ChargerMixin.java
@@ -4,16 +4,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.dries007.tfc.common.capabilities.food.FoodCapability;
 import net.dries007.tfc.common.entities.livestock.horse.HorseProperties;
+import net.dries007.tfc.common.items.Food;
+import net.dries007.tfc.common.items.TFCItems;
 import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.calendar.Calendars;
 import net.dries007.tfc.util.calendar.ICalendar;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.InteractionHand;
@@ -21,24 +23,26 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnGroupData;
-import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
-import net.wanmine.wab.entity.Soarer;
+import net.wanmine.wab.entity.Charger;
+import net.wanmine.wab.init.world.WabSounds;
 
 import su.terrafirmagreg.core.common.data.TFGTags;
-import su.terrafirmagreg.core.common.data.entities.soarer.SoarerData;
+import su.terrafirmagreg.core.common.data.entities.charger.ChargerData;
 
-@Mixin(value = Soarer.class)
-public abstract class SoarerMixin extends TamableAnimal {
-
+@Mixin(value = Charger.class)
+public abstract class ChargerMixin extends AbstractHorse {
     @Unique
     private long tfg$nextFeedTime = Long.MIN_VALUE;
 
-    protected SoarerMixin(EntityType<? extends TamableAnimal> pEntityType, Level pLevel) {
+    protected ChargerMixin(EntityType<? extends AbstractHorse> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
@@ -50,8 +54,8 @@ public abstract class SoarerMixin extends TamableAnimal {
 
     @Inject(method = "defineSynchedData", at = @At("TAIL"))
     private void tfg$defineSyncedData(CallbackInfo ci) {
-        entityData.define(SoarerData.DATA_IS_MALE, true);
-        entityData.define(SoarerData.DATA_FAMILIARITY, 0f);
+        entityData.define(ChargerData.DATA_IS_MALE, true);
+        entityData.define(ChargerData.DATA_FAMILIARITY, 0f);
     }
 
     @Inject(method = "readAdditionalSaveData", at = @At("TAIL"))
@@ -70,33 +74,40 @@ public abstract class SoarerMixin extends TamableAnimal {
 
     @Unique
     public float tfg$getFamiliarity() {
-        return entityData.get(SoarerData.DATA_FAMILIARITY);
+        return entityData.get(ChargerData.DATA_FAMILIARITY);
     }
 
     @Unique
     public void tfg$setFamiliarity(float familiarity) {
-        entityData.set(SoarerData.DATA_FAMILIARITY, familiarity);
+        entityData.set(ChargerData.DATA_FAMILIARITY, familiarity);
     }
 
     @Unique
     public void tfg$setIsMale(boolean male) {
-        entityData.set(SoarerData.DATA_IS_MALE, male);
+        entityData.set(ChargerData.DATA_IS_MALE, male);
     }
 
     @Unique
     public boolean tfg$isMale() {
-        return entityData.get(SoarerData.DATA_IS_MALE);
+        return entityData.get(ChargerData.DATA_IS_MALE);
     }
 
-    @Inject(method = "canMate", at = @At("HEAD"), cancellable = true)
-    public void tfg$canMate(Animal pOtherAnimal, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(false);
+    @Inject(method = "spawnChildFromBreeding", at = @At("HEAD"), cancellable = true)
+    public void tfg$spawnChildFromBreeding(ServerLevel level, Animal mate, CallbackInfo ci) {
+        ci.cancel();
     }
 
-    // Soarers eat the same thing as Surfers
+    @Inject(method = "registerGoals", at = @At("TAIL"))
+    public void tfg$registerGoals(CallbackInfo ci) {
+        this.goalSelector.removeAllGoals(g -> g instanceof TemptGoal);
+        this.goalSelector.addGoal(5, new TemptGoal(this, 1.0F, Ingredient.of(TFCItems.FOOD.get(Food.BEET).get()), false));
+        this.goalSelector.addGoal(5, new TemptGoal(this, 1.0F, Ingredient.of(TFGTags.Items.MartianHerbivoreFoods), false));
+    }
+
+    // Chargers eat the same thing as wraptors
     @Inject(method = "isFood", at = @At("HEAD"), cancellable = true)
     public void tfg$isFood(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(!FoodCapability.isRotten(stack) && Helpers.isItem(stack, TFGTags.Items.MartianCarnivoreFoods));
+        cir.setReturnValue(!FoodCapability.isRotten(stack) && Helpers.isItem(stack, TFGTags.Items.MartianHerbivoreFoods));
     }
 
     @Inject(method = "mobInteract", at = @At("HEAD"), cancellable = true)
@@ -112,32 +123,31 @@ public abstract class SoarerMixin extends TamableAnimal {
                     playSound(SoundEvents.PLAYER_BURP);
 
                     // If it's now familiar enough, set the owner
-                    if (isTame()) {
-                        tame(player);
+                    if (isTamed()) {
+                        setTamed(true);
+                        setOwnerUUID(player.getUUID());
                     }
                 }
             }
 
-            // Extra food restores flaps and heals
-            if (isTame()) {
-                ((Soarer) (Object) this).addFlaps(10);
+            // Extra food heals
+            if (isTamed()) {
                 this.heal(4.0F);
                 usePlayerItem(player, hand, held);
                 playSound(SoundEvents.PLAYER_BURP);
             }
 
             cir.setReturnValue(InteractionResult.sidedSuccess(this.level().isClientSide));
+        } else if (held.is(TFCItems.FOOD.get(Food.BEET).get())) {
+            this.usePlayerItem(player, hand, held);
+            ((Charger) (Object) this).setAggressive();
+            this.playSound(WabSounds.CHARGER_AGGRESSIVE.get(), this.getSoundVolume() * 3.0F, this.getVoicePitch());
+            cir.setReturnValue(InteractionResult.sidedSuccess(this.level().isClientSide));
         }
     }
 
     @Override
-    public boolean isTame() {
+    public boolean isTamed() {
         return tfg$getFamiliarity() > HorseProperties.TAMED_FAMILIARITY;
-    }
-
-    // Buffs how far they can glide for
-    @ModifyArg(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/wanmine/wab/entity/Soarer;setDeltaMovement(DDD)V", ordinal = 2), index = 1)
-    public double tfg$tick(double par1) {
-        return par1 / 1.5;
     }
 }
