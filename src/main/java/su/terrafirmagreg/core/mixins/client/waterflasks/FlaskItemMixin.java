e071586b9fe5e479ce5584002451de88e2cfb038
Ujhik <ujhik@usal.es>
2026-02-09 23:56:31 +0100

    fix: Correct code with improved injection

--- src/main/java/su/terrafirmagreg/core/mixins/client/waterflasks/FlaskItemMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/client/waterflasks/FlaskItemMixin.java
@@ -1,23 +1,21 @@
 package su.terrafirmagreg.core.mixins.client.waterflasks;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.dries007.tfc.common.capabilities.Capabilities;
 import net.dries007.tfc.common.items.DiscreteFluidContainerItem;
 import net.dries007.tfc.util.Drinkable;
 import net.dries007.tfc.util.Helpers;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import org.labellum.mc.waterflasks.item.FlaskItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 import java.util.function.Supplier;
 
@@ -28,22 +26,45 @@ public abstract class FlaskItemMixin extends DiscreteFluidContainerItem {
         super(properties, capacity, whitelist, canPlaceLiquidsInWorld, canPlaceSourceBlocks);
     }
 
+    // spotless:off
     /**
      * @author Ujhik
      * @reason Add tfc behavior enabling certain drinks like wines that give buffs to be drink when player has full water meter
      */
-    @Inject(method = "use", at = @At(value = "RETURN"), cancellable = true, remap = true)
-    private void tfg$useMayDrinkWhenFull(Level level, Player player, InteractionHand usedHand,
-            CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
+    @ModifyExpressionValue(
+            method = "use",
+            slice = @Slice(
+                    from = @At(
+                            value = "INVOKE",
+                            target = "Lnet/minecraft/world/entity/player/Player;getFoodData()Lnet/minecraft/world/food/FoodData;"
+                    ),
+                    to = @At(
+                            value = "INVOKE",
+                            target = "Lnet/minecraftforge/fluids/FluidStack;getAmount()I",
+                            remap = false
+                    )
+            ),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;get()Ljava/lang/Object;",
+                    ordinal = 0,
+                    remap = false
+            ),
+            remap = true
+    )
+    private Object modifyConfig(Object original, @Local(name = "player") Player player, @Local(name = "hand") InteractionHand hand) {
+        Boolean bOriginal = (Boolean) original;
 
-        ItemStack stack = player.getItemInHand(usedHand);
+        ItemStack stack = player.getItemInHand(hand);
         final IFluidHandler handler = Helpers.getCapability(stack, Capabilities.FLUID_ITEM);
-        var result = cir.getReturnValue().getResult();
-        if (handler != null && result == InteractionResult.FAIL) {
-            final Drinkable drinkable = Drinkable.get(handler.getFluidInTank(0).getFluid());
+        if (handler == null)
+            return bOriginal;
 
-            if (drinkable != null && drinkable.mayDrinkWhenFull())
-                cir.setReturnValue(afterEmptyFailed(handler, level, player, stack, usedHand));
-        }
+        final Drinkable drinkable = Drinkable.get(handler.getFluidInTank(0).getFluid());
+        if (drinkable == null)
+            return bOriginal;
+
+        return bOriginal || drinkable.mayDrinkWhenFull();
     }
+    // spotless:on
 }
