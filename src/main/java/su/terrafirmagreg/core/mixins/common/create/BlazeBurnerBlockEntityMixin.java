781e7819be7ccf3ca2f5d916db43d288b101f5ce
Mqrius <Nuntius.Marii@Gmail.com>
2026-02-10 04:42:01 +0100

    Fix liquids still being inserted from pipes

--- src/main/java/su/terrafirmagreg/core/mixins/common/create/BlazeBurnerBlockEntityMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/create/BlazeBurnerBlockEntityMixin.java
@@ -3,8 +3,10 @@ package su.terrafirmagreg.core.mixins.common.create;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.bawnorton.mixinsquared.TargetHandler;
 import com.simibubi.create.content.processing.burner.BlazeBurnerBlockEntity;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
 
@@ -15,9 +17,9 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 /**
- * Mixin to prevent any fuel from being burned in the Blaze Burner, by hand, liquid, or mechanical arm.
- * Needs higher priority than CreateLiquidFuel's mixin
- * Fuel still enters, but won't be burned.
+ * Mixin to prevent fuel from being inserted in the Blaze Burner by hand or mechanical arm in airless dimensions.
+ * Needs higher priority than CreateLiquidFuel's mixin to prevent liquid insertion with a bucket.
+ * Automated liquid insertion with pipes etc still works, but won't burn.
  */
 @Mixin(value = BlazeBurnerBlockEntity.class, priority = 900, remap = false)
 public abstract class BlazeBurnerBlockEntityMixin extends SmartBlockEntity {
@@ -31,6 +33,21 @@ public abstract class BlazeBurnerBlockEntityMixin extends SmartBlockEntity {
         assert level != null;
         if (level.dimension() != Level.OVERWORLD && level.dimension() != Level.NETHER) {
             cir.setReturnValue(false);
+        }
+    }
+
+    /**
+     * Prevent inserted liquid fuel from being burned
+     * MixinSquared, targeting Create Liquid Fuel's tick method
+     * Exits out of their mixin if we're not in the overworld/beneath.
+     */
+    @SuppressWarnings("CancellableInjectionUsage") // Idea gets confused about the two CallbackInfos
+    @TargetHandler(mixin = "com.forsteri.createliquidfuel.mixin.MixinBlazeBurnerTileEntity", name = "tick")
+    @Inject(method = "@MixinSquared:Handler", at = @At("HEAD"), cancellable = true)
+    private void tfg$cancelLiquidFuelTick(CallbackInfo originalCi, CallbackInfo ci) {
+        assert level != null;
+        if (level.dimension() != Level.OVERWORLD && level.dimension() != Level.NETHER) {
+            ci.cancel();
         }
     }
 }
