732bc98ff43635ac91aa6e00f8f7d49057ad2493
Ryan Cooper <45916709+Bumperdo09@users.noreply.github.com>
2026-02-11 22:50:51 -0500

    Make face mask curio slot work

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/ProtectionTypeMixin.java
@@ -0,0 +1,19 @@
+package su.terrafirmagreg.core.mixins.common.gtceu;
+
+import java.util.Set;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.gregtechceu.gtceu.api.data.chemical.material.properties.HazardProperty.ProtectionType;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
+@Mixin(value = ProtectionType.class, remap = false)
+public abstract class ProtectionTypeMixin {
+
+    //Change mask curio slot from "head" to "face"
+    @ModifyExpressionValue(method = "<clinit>", at = @At(value = "INVOKE", target = "Ljava/util/Set;of(Ljava/lang/Object;)Ljava/util/Set;", ordinal = 0))
+    private static Set<String> modifyMaskCurioSlot(Set<String> original) {
+        return Set.of("face");
+    }
+}
