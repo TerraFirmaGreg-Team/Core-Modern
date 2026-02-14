f4cd4807bcb5d08c776293e3ec53513a2648a10a
Ujhik <ujhik@usal.es>
2026-02-14 10:48:18 +0100

    fix: null protection and mixin adjustments

--- src/main/java/su/terrafirmagreg/core/mixins/common/sacks_n_such/AbstractArrowMixinSquared.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/sacks_n_such/AbstractArrowMixinSquared.java
@@ -20,15 +20,16 @@ public abstract class AbstractArrowMixinSquared {
      * @author Ujhik
      * @reason To correct the arrow dupe bug that happens when a player has and stack of arrows in the inventory, no quiver, and picks up a shoot arrow from the ground.
     */
+    @SuppressWarnings("CancellableInjectionUsage")
     @TargetHandler(mixin = "mod.traister101.sns.mixins.common.AbstractArrowMixin", name = "tryInsertIntoQuiver")
-    @Inject(method = "@MixinSquared:Handler", at = @At("RETURN"), cancellable = true)
-    private void modifyReturnBasedOnRemainder(
+    @Inject(method = "@MixinSquared:Handler", at = @At("RETURN"))
+    private void tfg$modifyReturnBasedOnRemainder(
             Player player,
             CallbackInfoReturnable<Boolean> cir,
             CallbackInfo ci,
             @SuppressWarnings("LocalMayBeArgsOnly") @Local(name = "inventoryRemainder") ItemStack inventoryRemainder) {
 
-        if (inventoryRemainder.getCount() < 1) {
+        if (inventoryRemainder != null && inventoryRemainder.getCount() < 1) {
             cir.setReturnValue(true);
         }
     }
