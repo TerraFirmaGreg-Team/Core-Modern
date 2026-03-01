18d5a69a705a09e456b757acd057d3cdee80346a
Ujhik <ujhik@usal.es>
2026-03-01 20:25:52 +0100

    bugfix: code simplification

--- src/main/java/su/terrafirmagreg/core/mixins/client/tfc/MoldCapabilityMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/client/tfc/MoldCapabilityMixin.java
@@ -1,34 +1,20 @@
 package su.terrafirmagreg.core.mixins.client.tfc;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.dries007.tfc.common.capabilities.heat.HeatCapability;
-import net.dries007.tfc.common.capabilities.heat.HeatHandler;
-import net.dries007.tfc.util.Metal;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.capability.templates.FluidTank;
 
 @Mixin(targets = "net.dries007.tfc.common.items.MoldItem$MoldCapability", remap = false)
-public class MoldCapabilityMixin {
-    @Final
-    @Shadow
-    private FluidTank tank;
-
-    @Final
-    @Shadow
-    private HeatHandler heat;
+public abstract class MoldCapabilityMixin {
 
     @Shadow
-    private boolean initialized;
+    protected abstract void load();
 
     /**
      * @author Ujhik
@@ -36,19 +22,6 @@ public class MoldCapabilityMixin {
      */
     @Inject(method = "<init>(Lnet/minecraft/world/item/ItemStack;ILnet/minecraft/tags/TagKey;)V", at = @At("TAIL"), remap = false)
     private void onInit(ItemStack stack, int capacity, TagKey<Fluid> fluidTag, CallbackInfo ci) {
-        final CompoundTag tankTag = stack.getTagElement("tank");
-        if (tankTag != null) {
-            tank.readFromNBT(tankTag);
-            initialized = true;
-        }
-
-        // recalculate heat capacity based on actual fluid contents
-        final FluidStack fluid = tank.getFluid();
-        final Metal metal = Metal.get(fluid.getFluid());
-        float value = HeatCapability.POTTERY_HEAT_CAPACITY;
-        if (!fluid.isEmpty() && metal != null) {
-            value += metal.getHeatCapacity(fluid.getAmount());
-        }
-        heat.setHeatCapacity(value);
+        this.load();
     }
 }
