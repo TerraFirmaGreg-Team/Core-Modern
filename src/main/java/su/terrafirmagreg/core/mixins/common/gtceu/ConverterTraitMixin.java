dcbb8dedf4fdd613c91ff3b0192bb359585bb531
DrEthan <ethan.r.draper@gmail.com>
2026-02-28 23:07:23 -0700

    Make Converters Pull FE
    For now this is limited in a slightly hacky way to the portable energy interface since everything else in Create Additions (Crafts & Additions) pushes FE. The Accumulator pushes FE when a converter is below it.

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/ConverterTraitMixin.java
@@ -0,0 +1,49 @@
+package su.terrafirmagreg.core.mixins.common.gtceu;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.gregtechceu.gtceu.api.capability.GTCapabilityHelper;
+import com.gregtechceu.gtceu.api.capability.compat.FeCompat;
+import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.api.machine.trait.NotifiableEnergyContainer;
+import com.gregtechceu.gtceu.common.machine.trait.ConverterTrait;
+
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mixin(ConverterTrait.class)
+public abstract class ConverterTraitMixin extends NotifiableEnergyContainer {
+
+    public ConverterTraitMixin(MetaMachine machine, long maxCapacity, long maxInputVoltage, long maxInputAmperage, long maxOutputVoltage, long maxOutputAmperage) {
+        super(machine, maxCapacity, maxInputVoltage, maxInputAmperage, maxOutputVoltage, maxOutputAmperage);
+    }
+
+    @Inject(method = "serverTick", at = @At(value = "INVOKE", target = "Lcom/gregtechceu/gtceu/api/machine/trait/NotifiableEnergyContainer;serverTick()V"), remap = false)
+    private void tryFeExtract(CallbackInfo ci) {
+        var frontFacing = machine.getFrontFacing();
+        var thisEnergyContainer = GTCapabilityHelper.getForgeEnergy(machine.getLevel(),
+                machine.getPos(), null);
+        for (Direction d : Direction.values()) {
+            if (d == frontFacing)
+                continue;
+            BlockState state = machine.getLevel().getBlockState(machine.getPos().relative(d));
+            ResourceLocation id = ForgeRegistries.BLOCKS.getKey(state.getBlock());
+            var targetEnergyContainer = GTCapabilityHelper.getForgeEnergy(machine.getLevel(),
+                    machine.getPos().relative(d), null);
+            if (targetEnergyContainer != null && targetEnergyContainer.canExtract() && id.getPath().contains("portable_energy_interface")) {
+                int energyExtracted = targetEnergyContainer.extractEnergy(
+                        thisEnergyContainer.receiveEnergy(
+                                FeCompat.toFe(
+                                        getEnergyCapacity() - getEnergyStored(), FeCompat.ratio(false)),
+                                true),
+                        false);
+                thisEnergyContainer.receiveEnergy(energyExtracted, false);
+            }
+        }
+    }
+}
