31589d2490a55e65ffeb211411a9f78bd8b09217
TomPlop <tomdidome@gmail.com>
2026-03-04 19:31:19 -0500

    Add customStill to some materials to change the textures

---
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGModifyMaterials.java
@@ -0,0 +1,26 @@
+package su.terrafirmagreg.core.common.data;
+
+import com.gregtechceu.gtceu.api.data.chemical.material.Material;
+import com.gregtechceu.gtceu.api.data.chemical.material.event.PostMaterialEvent;
+import com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey;
+import com.gregtechceu.gtceu.api.fluids.FluidBuilder;
+import com.gregtechceu.gtceu.api.fluids.store.FluidStorageKeys;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
+
+public class TFGModifyMaterials {
+
+    public static void modify(PostMaterialEvent event) {
+        enableCustomStill("iron_iii_chloride");
+        enableCustomStill("soldering_alloy");
+        enableCustomStill("tin");
+    }
+
+    private static void enableCustomStill(String materialName) {
+        Material material = GTMaterials.get(materialName);
+
+        FluidBuilder builder = material.getProperty(PropertyKey.FLUID).getQueuedBuilder(FluidStorageKeys.LIQUID);
+        if (builder != null) {
+            builder.textures(true);
+        }
+    }
+}
