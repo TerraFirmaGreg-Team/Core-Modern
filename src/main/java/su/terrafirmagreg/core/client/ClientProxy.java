9299366352dc54a93e23e64f385f0b4f626ef6c4
Mqrius <Nuntius.Marii@Gmail.com>
2026-03-06 23:46:47 +0100

    Unused imports delete

--- src/main/java/su/terrafirmagreg/core/client/ClientProxy.java
+++ src/main/java/su/terrafirmagreg/core/client/ClientProxy.java
@@ -1,7 +1,6 @@
 package su.terrafirmagreg.core.client;
 
 import static earth.terrarium.adastra.client.forge.AdAstraClientForge.ITEM_RENDERERS;
-import static su.terrafirmagreg.core.common.data.TFGEntities.*;
 
 import java.util.function.BiConsumer;
 
@@ -19,7 +18,6 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import earth.terrarium.adastra.client.models.entities.vehicles.RocketModel;
