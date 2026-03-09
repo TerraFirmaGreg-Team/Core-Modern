de41f15645560e7530f7c2121ce363af997d4c00
Pyritie <pyritie@gmail.com>
2026-03-09 12:12:20 +0000

    turns out we need hardened/cobble tuff

--- src/main/java/su/terrafirmagreg/core/common/data/TFGBlocks_Earth.java
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGBlocks_Earth.java
@@ -10,6 +10,7 @@ import com.tterrag.registrate.util.nullness.NonNullBiConsumer;
 import net.dries007.tfc.common.TFCTags;
 import net.dries007.tfc.common.blocks.TFCBlocks;
 import net.dries007.tfc.common.blocks.soil.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.BlockItem;
@@ -39,6 +40,26 @@ public class TFGBlocks_Earth {
     public static BlockEntry<ConnectedDuffBlock> LOAM_DUFF;
 
     // New TFC Worldgen
+    public static final BlockEntry<Block> HARDENED_TUFF = TFGCore.REGISTRATE.block("hardened_tuff", Block::new)
+            .initialProperties(() -> Blocks.TUFF)
+            .exBlockstate(GTModels.cubeAllModel(ResourceLocation.fromNamespaceAndPath("minecraft", "block/tuff")))
+            .properties(p -> p.mapColor(MapColor.TERRACOTTA_GRAY))
+            .tag(Tags.Blocks.STONE, TFCTags.Blocks.CAN_CARVE, BlockTags.MINEABLE_WITH_PICKAXE)
+            .item(BlockItem::new)
+            .tag(Tags.Items.STONE)
+            .build()
+            .register();
+
+    public static final BlockEntry<Block> TUFF_COBBLE = TFGCore.REGISTRATE.block("tuff_cobble", Block::new)
+            .initialProperties(() -> Blocks.TUFF)
+            .exBlockstate(GTModels.cubeAllModel(TFGCore.id("block/tuff_cobble")))
+            .properties(p -> p.mapColor(MapColor.TERRACOTTA_GRAY))
+            .tag(Tags.Blocks.COBBLESTONE, TFCTags.Blocks.CAN_CARVE, TFCTags.Blocks.CAN_LANDSLIDE, BlockTags.MINEABLE_WITH_PICKAXE)
+            .item(BlockItem::new)
+            .tag(Tags.Items.COBBLESTONE)
+            .build()
+            .register();
+
     public static final BlockEntry<Block> TUFF_GRAVEL = TFGCore.REGISTRATE.block("tuff_gravel", Block::new)
             .initialProperties(() -> Blocks.GRAVEL)
             .exBlockstate(GTModels.cubeAllModel(TFGCore.id("block/tuff_gravel")))
