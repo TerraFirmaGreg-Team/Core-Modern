d7a04ea98d531820628ea344452d1bab83998b46
Pyritie <pyritie@gmail.com>
2026-03-21 13:22:27 +0000

    make gem buds no longer visible on the map

--- src/main/java/su/terrafirmagreg/core/common/data/TFGBlocks_Buds.java
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGBlocks_Buds.java
@@ -19,6 +19,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.storage.loot.LootTable;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
@@ -68,6 +69,7 @@ public class TFGBlocks_Buds {
                         .noCollission()
                         .strength(0.25f)
                         .lightLevel(b -> 3)
+						.mapColor(MapColor.NONE)
                         .offsetType(BlockBehaviour.OffsetType.XZ))
                 .setData(ProviderType.LANG, NonNullBiConsumer.noop())
                 .setData(ProviderType.LOOT, NonNullBiConsumer.noop())
