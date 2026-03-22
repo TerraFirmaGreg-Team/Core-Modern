31ecbdcbf00cd8477f5ca5f23d34f3ce4dd0e845
Pyritie <pyritie@gmail.com>
2026-03-22 14:50:01 +0000

    spotless

--- src/main/java/su/terrafirmagreg/core/common/data/TFGBlocks_Buds.java
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGBlocks_Buds.java
@@ -19,7 +19,6 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.storage.loot.LootTable;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
