83e4f926be3c945f3e537e9496776920836210f7
TomPlop <tomdidome@gmail.com>
2026-03-22 20:22:41 -0400

    test mixin fluid vein

---
+++ src/main/java/su/terrafirmagreg/core/common/data/tfgt/worldgen/ClimateWeightModifier.java
@@ -0,0 +1,47 @@
+package su.terrafirmagreg.core.common.data.tfgt.worldgen;
+
+import net.dries007.tfc.util.climate.Climate;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+
+public class ClimateWeightModifier {
+
+    public enum Mode {
+        TEMPERATURE, RAINFALL
+    }
+
+    private final Mode mode;
+    private final float min;
+    private final float max;
+    private final int addedWeight;
+
+    public ClimateWeightModifier(Mode mode, float min, float max, int addedWeight) {
+        this.mode = mode;
+        this.min = min;
+        this.max = max;
+        this.addedWeight = addedWeight;
+    }
+
+    public int applyAsInt(ServerLevel level, BlockPos pos) {
+        float value = mode == Mode.TEMPERATURE
+                ? Climate.getAverageTemperature(level, pos)
+                : Climate.getRainfall(level, pos);
+        return value >= min && value <= max ? addedWeight : 0;
+    }
+
+    public Mode getMode() {
+        return mode;
+    }
+
+    public float getMin() {
+        return min;
+    }
+
+    public float getMax() {
+        return max;
+    }
+
+    public int getAddedWeight() {
+        return addedWeight;
+    }
+}
