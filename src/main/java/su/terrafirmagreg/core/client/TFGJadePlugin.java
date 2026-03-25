72acf6dfefcd3b78976f9ecc78dc57d6017e7ae1
DrEthan <20546072+DrEthan77@users.noreply.github.com>
2026-03-25 12:29:36 -0600

    Changed to use TFG Mod_ID

--- src/main/java/su/terrafirmagreg/core/client/TFGJadePlugin.java
+++ src/main/java/su/terrafirmagreg/core/client/TFGJadePlugin.java
@@ -6,12 +6,13 @@ import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
 
+import su.terrafirmagreg.core.TFGCore;
 import su.terrafirmagreg.core.common.data.TierLockedBlock;
 
 @WailaPlugin
 public class TFGJadePlugin implements IWailaPlugin {
 
-    public static final ResourceLocation TLB_Info = ResourceLocation.fromNamespaceAndPath("tfg_core", "tier_locked_block_info");
+    public static final ResourceLocation TLB_Info = ResourceLocation.fromNamespaceAndPath(TFGCore.MOD_ID, "tier_locked_block_info");
 
     @Override
     public void registerClient(IWailaClientRegistration registration) {
