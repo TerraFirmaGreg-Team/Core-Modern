0976f23ab144e7ff9afa94f6b35ccc119e28f207
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2026-03-28 12:36:32 -0500

    spotless ffs

--- src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/ProspectorModeMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/ProspectorModeMixin.java
@@ -1,6 +1,5 @@
 package su.terrafirmagreg.core.mixins.common.gtceu;
 
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
