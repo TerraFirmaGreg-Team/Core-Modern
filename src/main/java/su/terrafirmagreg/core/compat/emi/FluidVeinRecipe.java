4ed9f0bc76f9483e97f8b74ecd4ed56ded83bb79
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2026-04-03 23:41:16 -0500

    works more

--- src/main/java/su/terrafirmagreg/core/compat/emi/FluidVeinRecipe.java
+++ src/main/java/su/terrafirmagreg/core/compat/emi/FluidVeinRecipe.java
@@ -4,17 +4,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.Nullable;
 
 import com.gregtechceu.gtceu.api.data.worldgen.BiomeWeightModifier;
 import com.gregtechceu.gtceu.api.data.worldgen.bedrockfluid.BedrockFluidDefinition;
+import com.gregtechceu.gtceu.api.data.worldgen.bedrockfluid.BedrockFluidVeinSavedData;
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
-import com.gregtechceu.gtceu.common.data.GTItems;
 
-import net.minecraft.core.Holder;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.core.HolderSet;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
@@ -92,7 +94,7 @@ public class FluidVeinRecipe implements EmiRecipe {
 
     @Override
     public List<EmiIngredient> getInputs() {
-        return List.of(EmiStack.of(GTItems.PROSPECTOR_HV));
+        return List.of();
     }
 
     @Override
@@ -102,12 +104,12 @@ public class FluidVeinRecipe implements EmiRecipe {
 
     @Override
     public int getDisplayWidth() {
-        return 140;
+        return 168;
     }
 
     @Override
     public int getDisplayHeight() {
-        return 180;
+        return 170;
     }
 
     private final int fluidSize = 24;
@@ -120,14 +122,16 @@ public class FluidVeinRecipe implements EmiRecipe {
 
         yPointer = addTank(widgets, this.getDisplayWidth() / 2 - fluidSize / 2, yPointer);
 
-        yPointer = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.yield_range", minYield, maxYield), 2, yPointer);
+        yPointer = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.yield_range", minYield + "mB/s", maxYield + "mB/s"), 2, yPointer);
 
-        //Add tooltip that shows depletion amount
-        yPointer = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.depletion", depChance + "%"), 2, yPointer);
+        yPointer = addTextLineTooltip(widgets, Component.translatable("tfg.emi.fluid_veins.depletion", depChance + "%"), 2, yPointer,
+                Component.translatable("tfg.emi.fluid_veins.depletion.tooltip", depChance + "%", depAmount, BedrockFluidVeinSavedData.MAXIMUM_VEIN_OPERATIONS));
 
-        yPointer = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.depleted_yield", depYield), 2, yPointer);
+        yPointer = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.depleted_yield", depYield + "mB/s"), 2, yPointer);
 
-        yPointer = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.weight", weight), 2, yPointer);
+        if (weight != 0) {
+            yPointer = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.weight", weight), 2, yPointer);
+        }
 
         yPointer = addDimensionIcons(widgets, 2, yPointer);
 
@@ -152,7 +156,7 @@ public class FluidVeinRecipe implements EmiRecipe {
         pointerX += (textWidget.getBounds().width() + 2);
 
         for (var dimension : dimensions) {
-            //slot is 18px tall, text is 8px tall
+            //slot is 16px tall, text is 8px tall
             var slotWidget = new SlotWidget(EmiStack.of(Objects.requireNonNull(GTRegistries.DIMENSION_MARKERS.get(dimension.location())).getIcon()),
                     pointerX, y + 4 - 8);
             slotWidget.drawBack(false);
@@ -165,20 +169,11 @@ public class FluidVeinRecipe implements EmiRecipe {
 
     private int addBiomes(WidgetHolder widgets, int x, int y) {
 
-        widgets.addText(Component.translatable("tfg.emi.fluid_veins.biomes"), x, y, 0, false);
-        int indent = 6;
-
-        if (biomeHolder.size() == 0 && climateWeight == null) {
-            y += 8;
-            return addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.biome_any"), x + indent, y);
+        if (climateWeight == null) {
+            y = addBiomeList(widgets, biomeHolder.stream().map(entry -> entry.unwrapKey().get()).collect(Collectors.toSet()), x, y, biomeHolder.size() == 0);
         }
 
-        for (Holder<Biome> entry : biomeHolder) {
-            y += 8;
-            addTextLine(widgets, Component.translatable("biome." + entry.unwrapKey().get().location().toLanguageKey()), x + indent, y);
-        }
-
-        return newLineY(y);
+        return y;
     }
 
     private int addClimate(WidgetHolder widgets, int x, int y) {
@@ -187,24 +182,12 @@ public class FluidVeinRecipe implements EmiRecipe {
         }
 
         for (var climateDef : climateWeight) {
-            widgets.addText(Component.translatable("tfg.emi.fluid_veins.biomes"), x, y, 0, false);
-            int indent = 6;
-
-            if (climateDef.getBiomes() != null) {
-                for (ResourceKey<Biome> entry : climateDef.getBiomes()) {
-                    y += 8;
-                    addTextLine(widgets, Component.translatable("biome." + entry.location().toLanguageKey()), x + indent, y);
-                }
-                y = newLineY(y);
-            } else {
-                y += 8;
-                y = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.biome_any"), x + indent, y);
-            }
+            y = addBiomeList(widgets, climateDef.getBiomes(), x, y, (climateDef.getBiomes() == null));
 
             for (var climateVar : climateDef.getClimates().entrySet()) {
                 String type = climateVar.getKey();
-                float min = climateVar.getValue().get(0);
-                float max = climateVar.getValue().get(1);
+                float min = climateVar.getValue().get(0).intValue();
+                float max = climateVar.getValue().get(1).intValue();
 
                 String numberSuffix = "temperature".equals(type) ? "°C" : "mm";
 
@@ -214,8 +197,62 @@ public class FluidVeinRecipe implements EmiRecipe {
         return y;
     }
 
+    private int addBiomeList(WidgetHolder widgets, Set<ResourceKey<Biome>> biomeKeys, int x, int y, boolean anyBiome) {
+        int indent = 6;
+        int cutoff = 6;
+        widgets.addText(Component.translatable("tfg.emi.fluid_veins.biomes"), x, y, 0, false);
+
+        if (!anyBiome) {
+            int i = 0;
+
+            MutableComponent tooltip = Component.empty();
+            for (ResourceKey<Biome> entry : biomeKeys) {
+                i++;
+                var langComp = Component.translatable("biome." + entry.location().toLanguageKey());
+
+                if (i < cutoff) {
+                    y += 8;
+                    addTextLine(widgets, langComp, x + indent, y);
+                }
+
+                String appendSuffix = i == biomeKeys.size() ? "" : ", ";
+                tooltip.append(langComp).append(appendSuffix);
+            }
+
+            if (i >= cutoff) {
+                y += 8;
+                var overflowText = new TextWidget(Component.translatable("tfg.emi.fluid_veins.biomes_overflow", "+" + (i - cutoff + 1)).getVisualOrderText(), x + indent, y, 0, false) {
+                    @Override
+                    public List<ClientTooltipComponent> getTooltip(int mouseX, int mouseY) {
+                        return List.of(ClientTooltipComponent.create(tooltip.getVisualOrderText()));
+                    }
+                };
+                widgets.add(overflowText);
+            }
+
+            y = newLineY(y);
+        } else {
+            y += 8;
+            y = addTextLine(widgets, Component.translatable("tfg.emi.fluid_veins.biome_any"), x + indent, y);
+        }
+
+        return y;
+    }
+
     private int addTextLine(WidgetHolder widgets, Component text, int x, int y) {
         widgets.addText(text, x, y, 0, false);
+        return newLineY(y);
+    }
+
+    private int addTextLineTooltip(WidgetHolder widgets, Component text, int x, int y, Component tooltip) {
+        var textWidget = new TextWidget(text.getVisualOrderText(), x, y, 0, false) {
+            @Override
+            public List<ClientTooltipComponent> getTooltip(int mouseX, int mouseY) {
+                return List.of(ClientTooltipComponent.create(tooltip.getVisualOrderText()));
+            }
+        };
+
+        widgets.add(textWidget);
         return newLineY(y);
     }
 
