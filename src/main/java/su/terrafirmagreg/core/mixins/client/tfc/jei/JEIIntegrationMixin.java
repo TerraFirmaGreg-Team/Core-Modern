bdf5ad1304faa932225c760b0acb4f0bd38d5a70
Redeix <brayden.j.m.ford@gmail.com>
2026-04-14 00:21:29 -0500

    New screen who dis?

---
+++ src/main/java/su/terrafirmagreg/core/mixins/client/tfc/jei/JEIIntegrationMixin.java
@@ -0,0 +1,31 @@
+/*
+ * This file includes code from TerraFirmaCraft (https://github.com/TerraFirmaCraft/TerraFirmaCraft)
+ * Copyright (c) 2020 alcatrazEscapee
+ * Licensed under the EUPLv1.2 License
+ */
+package su.terrafirmagreg.core.mixins.client.tfc.jei;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.dries007.tfc.compat.jei.JEIIntegration;
+import net.dries007.tfc.compat.jei.TFCInventoryGuiHandler;
+
+import mezz.jei.api.gui.handlers.IGuiContainerHandler;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+
+import su.terrafirmagreg.core.client.screen.TFGNutritionScreen;
+
+@Mixin(value = JEIIntegration.class, remap = false)
+public abstract class JEIIntegrationMixin {
+
+    /**
+     * Replaces TFC's NutritionScreen with TFGNutritionScreen in JEI/EMI GUI handler registration.
+     */
+    @Redirect(method = "registerGuiHandlers", at = @At(value = "INVOKE", target = "Lmezz/jei/api/registration/IGuiHandlerRegistration;addGuiContainerHandler(Ljava/lang/Class;Lmezz/jei/api/gui/handlers/IGuiContainerHandler;)V", ordinal = 3), remap = false)
+    private void tfg$replaceNutritionScreenHandler(IGuiHandlerRegistration registry, Class<?> screenClass, IGuiContainerHandler<?> handler) {
+        // Replace with our custom screen.
+        registry.addGuiContainerHandler(TFGNutritionScreen.class, new TFCInventoryGuiHandler<>());
+    }
+}
