f5ee3fa587496dd76d91825f128702383135b183
Sakura <sakura.kitsurugi@proton.me>
2026-04-20 00:07:51 +0300

    remove wrong branch stuff

--- src/main/java/su/terrafirmagreg/core/mixins/common/tfc/EnvironmentHelpersMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/tfc/EnvironmentHelpersMixin.java
@@ -6,9 +6,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
-
 import net.dries007.tfc.util.EnvironmentHelpers;
 import net.dries007.tfc.util.Helpers;
 import net.minecraft.server.level.ServerLevel;
@@ -19,7 +16,6 @@ import net.minecraft.world.level.chunk.LevelChunk;
 
 import earth.terrarium.adastra.api.planets.PlanetApi;
 
-import su.terrafirmagreg.core.common.data.TFGTags;
 import su.terrafirmagreg.core.common.data.blocks.TFGBlocks;
 
 @Mixin(value = EnvironmentHelpers.class, remap = false)
@@ -47,12 +43,4 @@ public abstract class EnvironmentHelpersMixin {
         }
     }
 
-    /**
-     * Prevents icicles from forming underneath blocks with 'tfg:no_icicle_generation' block tag
-     */
-
-    @ModifyExpressionValue(method = "doIcicles", at = @At(value = "INVOKE", target = "Lnet/dries007/tfc/util/Helpers;isBlock(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/tags/TagKey;)Z"))
-    private static boolean tfg$modifyTagCheck(boolean original, @Local(name = "stateAbove") BlockState stateAbove) {
-        return original || Helpers.isBlock(stateAbove, TFGTags.Blocks.NoIcicles);
-    }
 }
