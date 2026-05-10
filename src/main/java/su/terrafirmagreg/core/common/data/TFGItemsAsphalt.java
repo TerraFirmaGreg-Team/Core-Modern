b74d1d39bd99ace5bbdcbd423ad25509aceba48e
闫茂源 <115050813@qq.com>
2026-05-10 18:42:35 +0800

    feat: runData for asphalt items

--- src/main/java/su/terrafirmagreg/core/common/data/TFGItemsAsphalt.java
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGItemsAsphalt.java
@@ -2,7 +2,6 @@ package su.terrafirmagreg.core.common.data;
 
 import com.tterrag.registrate.util.entry.ItemEntry;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 
 import su.terrafirmagreg.core.TFGCore;
@@ -15,32 +14,37 @@ public final class TFGItemsAsphalt {
     public static void init() {
     }
 
-    /** Drops from breaking {@code tfg:asphalt_road*} blocks (not pouring). */
     public static final ItemEntry<Item> ASPHALT_RUBBLE = TFGCore.REGISTRATE.item("asphalt_rubble", Item::new)
+            .properties(p -> p.stacksTo(32))
+            .defaultModel()
+            .register();
+
+    public static final ItemEntry<Item> ASPHALT_BINDER = TFGCore.REGISTRATE.item("asphalt_binder", Item::new)
             .properties(p -> p.stacksTo(64))
-            .model((ctx, prov) -> prov.withExistingParent(ctx.getName(), ResourceLocation.withDefaultNamespace("item/generated"))
-                    .texture("layer0", TFGCore.id("item/" + ctx.getName())))
+            .defaultModel()
+            .register();
+
+    public static final ItemEntry<Item> TAR_CHUNK = TFGCore.REGISTRATE.item("tar_chunk", Item::new)
+            .properties(p -> p.stacksTo(32))
+            .defaultModel()
             .register();
 
     public static final ItemEntry<RoadMarkingStencilItem> ASPHALT_ROAD_STENCIL_LINES = TFGCore.REGISTRATE
             .item("asphalt_road_stencil_lines", p -> new RoadMarkingStencilItem(p, AsphaltRoadStencilPattern.LINE))
             .properties(p -> p.stacksTo(16))
-            .model((ctx, prov) -> prov.withExistingParent(ctx.getName(), ResourceLocation.withDefaultNamespace("item/generated")).texture("layer0",
-                    TFGCore.id("item/" + ctx.getName())))
+            .defaultModel()
             .register();
 
     public static final ItemEntry<RoadMarkingStencilItem> ASPHALT_ROAD_STENCIL_CROSS = TFGCore.REGISTRATE
             .item("asphalt_road_stencil_cross", p -> new RoadMarkingStencilItem(p, AsphaltRoadStencilPattern.CROSS))
             .properties(p -> p.stacksTo(16))
-            .model((ctx, prov) -> prov.withExistingParent(ctx.getName(), ResourceLocation.withDefaultNamespace("item/generated")).texture("layer0",
-                    TFGCore.id("item/" + ctx.getName())))
+            .defaultModel()
             .register();
 
     public static final ItemEntry<RoadMarkingStencilItem> ASPHALT_ROAD_STENCIL_ARROW = TFGCore.REGISTRATE
             .item("asphalt_road_stencil_arrow", p -> new RoadMarkingStencilItem(p, AsphaltRoadStencilPattern.ARROW))
             .properties(p -> p.stacksTo(16))
-            .model((ctx, prov) -> prov.withExistingParent(ctx.getName(), ResourceLocation.withDefaultNamespace("item/generated")).texture("layer0",
-                    TFGCore.id("item/" + ctx.getName())))
+            .defaultModel()
             .register();
 
 }
--- src/main/java/su/terrafirmagreg/core/common/data/blocks/TFGBlocksAsphalt.java
+++ src/main/java/su/terrafirmagreg/core/common/data/blocks/TFGBlocksAsphalt.java
@@ -1,6 +1,7 @@
 package su.terrafirmagreg.core.common.data.blocks;
 
 import com.tterrag.registrate.providers.ProviderType;
+import com.tterrag.registrate.providers.loot.RegistrateBlockLootTables;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import com.tterrag.registrate.util.nullness.NonNullBiConsumer;
 
@@ -9,10 +10,17 @@ import net.dries007.tfc.common.blockentities.TFCBlockEntities;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyExplosionDecay;
+import net.minecraft.world.level.storage.loot.predicates.ExplosionCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 
 import su.terrafirmagreg.core.TFGCore;
 import su.terrafirmagreg.core.common.block.asphalt.AsphaltRoadBlock;
@@ -21,7 +29,9 @@ import su.terrafirmagreg.core.common.block.asphalt.AsphaltRoadPouringBlock;
 import su.terrafirmagreg.core.common.block.asphalt.AsphaltRoadSlabBlock;
 import su.terrafirmagreg.core.common.block.asphalt.AsphaltRoadStairsBlock;
 import su.terrafirmagreg.core.common.data.TFGBlockEntities;
+import su.terrafirmagreg.core.common.data.TFGItemsAsphalt;
 
+@SuppressWarnings("unused")
 public final class TFGBlocksAsphalt {
 
     public static void init() {
@@ -30,8 +40,9 @@ public final class TFGBlocksAsphalt {
     public static final BlockEntry<AsphaltRoadPouringBlock> ASPHALT_ROAD_POURING = TFGCore.REGISTRATE.block("asphalt_road_pouring",
             AsphaltRoadPouringBlock::new)
             .initialProperties(() -> Blocks.BLACK_CONCRETE)
-            .properties(p -> p.strength(-1.0F, 3600000.0F).sound(SoundType.MUD).mapColor(MapColor.COLOR_BLACK))
+            .properties(p -> p.strength(-1.0F, 3600000.0F).sound(SoundType.MUD).mapColor(MapColor.COLOR_BLACK).noLootTable())
             .setData(ProviderType.BLOCKSTATE, NonNullBiConsumer.noop())
+            .loot((prov, block) -> prov.add(block, LootTable.lootTable()))
             .item(BlockItem::new).setData(ProviderType.ITEM_MODEL, NonNullBiConsumer.noop()).build()
             .register();
 
@@ -41,6 +52,7 @@ public final class TFGBlocksAsphalt {
             .setData(ProviderType.BLOCKSTATE, NonNullBiConsumer.noop())
             .tag(BlockTags.MINEABLE_WITH_PICKAXE, TFCTags.Blocks.SUPPORTS_LANDSLIDE, TFCTags.Blocks.TOUGHNESS_2)
             .onRegister(block -> TFGBlockEntities.addValidBEBlock(TFCBlockEntities.TICK_COUNTER, block))
+            .loot(TFGBlocksAsphalt::asphaltLoot)
             .item(BlockItem::new).setData(ProviderType.ITEM_MODEL, NonNullBiConsumer.noop()).build()
             .register();
 
@@ -49,6 +61,7 @@ public final class TFGBlocksAsphalt {
             .properties(p -> p.strength(6f, 64).sound(SoundType.DEEPSLATE_TILES).mapColor(MapColor.COLOR_BLACK).requiresCorrectToolForDrops())
             .addLayer(() -> RenderType::cutout)
             .setData(ProviderType.BLOCKSTATE, NonNullBiConsumer.noop())
+            .loot(TFGBlocksAsphalt::asphaltLoot)
             .tag(BlockTags.MINEABLE_WITH_PICKAXE, TFCTags.Blocks.SUPPORTS_LANDSLIDE, TFCTags.Blocks.TOUGHNESS_2)
             .item(BlockItem::new).setData(ProviderType.ITEM_MODEL, NonNullBiConsumer.noop()).build()
             .register();
@@ -58,6 +71,7 @@ public final class TFGBlocksAsphalt {
             .initialProperties(ASPHALT_ROAD)
             .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
             .setData(ProviderType.BLOCKSTATE, NonNullBiConsumer.noop())
+            .loot(TFGBlocksAsphalt::asphaltLoot)
             .tag(BlockTags.MINEABLE_WITH_PICKAXE, BlockTags.STAIRS, TFCTags.Blocks.SUPPORTS_LANDSLIDE, TFCTags.Blocks.TOUGHNESS_2)
             .item(BlockItem::new).setData(ProviderType.ITEM_MODEL, NonNullBiConsumer.noop()).build()
             .register();
@@ -67,7 +81,18 @@ public final class TFGBlocksAsphalt {
             .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
             .addLayer(() -> RenderType::cutout)
             .setData(ProviderType.BLOCKSTATE, NonNullBiConsumer.noop())
+            .loot(TFGBlocksAsphalt::asphaltLoot)
             .tag(BlockTags.MINEABLE_WITH_PICKAXE, BlockTags.SLABS, TFCTags.Blocks.SUPPORTS_LANDSLIDE, TFCTags.Blocks.TOUGHNESS_2)
             .item(BlockItem::new).setData(ProviderType.ITEM_MODEL, NonNullBiConsumer.noop()).build()
             .register();
+
+    private static void asphaltLoot(RegistrateBlockLootTables prov, Block block) {
+        var entry = LootItem.lootTableItem(TFGItemsAsphalt.ASPHALT_RUBBLE.asItem());
+        prov.add(block, LootTable.lootTable()
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantValue.exactly(1))
+                        .add(entry.apply(ApplyExplosionDecay.explosionDecay()))
+                        .when(ExplosionCondition.survivesExplosion())));
+    }
+
 }
