824d6627778191a84cf284153bd423e18759b8df
闫茂源 <115050813@qq.com>
2026-05-10 19:02:44 +0800

    feat: move tar_chunk and asphalt_binder item to kubejs

--- src/main/java/su/terrafirmagreg/core/common/data/TFGItemsAsphalt.java
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGItemsAsphalt.java
@@ -19,16 +19,6 @@ public final class TFGItemsAsphalt {
             .defaultModel()
             .register();
 
-    public static final ItemEntry<Item> ASPHALT_BINDER = TFGCore.REGISTRATE.item("asphalt_binder", Item::new)
-            .properties(p -> p.stacksTo(64))
-            .defaultModel()
-            .register();
-
-    public static final ItemEntry<Item> TAR_CHUNK = TFGCore.REGISTRATE.item("tar_chunk", Item::new)
-            .properties(p -> p.stacksTo(32))
-            .defaultModel()
-            .register();
-
     public static final ItemEntry<RoadMarkingStencilItem> ASPHALT_ROAD_STENCIL_LINES = TFGCore.REGISTRATE
             .item("asphalt_road_stencil_lines", p -> new RoadMarkingStencilItem(p, AsphaltRoadStencilPattern.LINE))
             .properties(p -> p.stacksTo(16))
