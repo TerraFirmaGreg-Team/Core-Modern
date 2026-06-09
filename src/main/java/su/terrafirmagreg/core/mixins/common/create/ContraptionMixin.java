80e541bda3f9f8086a258cec0b327b4e8f2bd28a
Pyritie <pyritie@gmail.com>
2026-06-09 22:39:07 +0100

    cleanup formatting

--- src/main/java/su/terrafirmagreg/core/mixins/common/create/ContraptionMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/create/ContraptionMixin.java
@@ -1,6 +1,5 @@
 package su.terrafirmagreg.core.mixins.common.create;
 
-import net.minecraft.server.level.DistanceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,20 +19,23 @@ import su.terrafirmagreg.core.config.TFGConfig;
 @Mixin(value = Contraption.class, remap = false)
 public class ContraptionMixin {
 
+    // Writes a message in chat to nearby players when a contraption is formed in the beneath or on hot planets.
+    // Yes, this means it'll also announce to players setting up a tree farm or something. It's fine!
+
     @Inject(method = "onEntityCreated", at = @At("TAIL"), remap = false)
     private void tfg$onEntityCreated(AbstractContraptionEntity entity, CallbackInfo ci) {
         var player = Minecraft.getInstance().player;
         if (player != null && player.position().distanceTo(entity.position()) < 20) {
+
             var dim = entity.level().dimension();
             if (dim == Level.NETHER && TFGConfig.SERVER.enableBeneathMiningRestrictions.get()) {
-                final int y = TFGConfig.SERVER.disabledBeneathMiningYLevel.get();
 
-                player.sendSystemMessage(
-                        Component.translatable("tfg.clientmessage.nether_contraption_created", y));
+                final int y = TFGConfig.SERVER.disabledBeneathMiningYLevel.get();
+                player.sendSystemMessage(Component.translatable("tfg.clientmessage.nether_contraption_created", y));
 
             } else if ((dim == Planet.VENUS || dim == Planet.MERCURY) && TFGConfig.SERVER.enableHotPlanetMiningRestrictions.get()) {
-                player.sendSystemMessage(
-                        Component.translatable("tfg.clientmessage.hot_planet_contraption_created"));
+
+                player.sendSystemMessage(Component.translatable("tfg.clientmessage.hot_planet_contraption_created"));
             }
         }
     }
