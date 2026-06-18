89cf0bbf264da87974009c1ffa31f87e4ef78376
Pyritie <pyritie@gmail.com>
2026-06-18 01:01:59 +0100

    hangers now disengage if attacked

--- src/main/java/su/terrafirmagreg/core/mixins/common/species/CliffHangerMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/species/CliffHangerMixin.java
@@ -2,6 +2,10 @@ package su.terrafirmagreg.core.mixins.common.species;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.ninni.species.server.entity.mob.update_3.CliffHanger;
 import com.ninni.species.server.entity.mob.update_3.Hanger;
@@ -10,6 +14,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.PathfinderMob;
@@ -19,9 +24,13 @@ import net.minecraft.world.level.ServerLevelAccessor;
 
 @Mixin(value = CliffHanger.class, remap = false)
 public abstract class CliffHangerMixin extends Hanger {
+
     protected CliffHangerMixin(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
     }
+
+    @Shadow
+    public abstract void setAttached(boolean attached);
 
     @Override
     public boolean fireImmune() {
@@ -37,5 +46,12 @@ public abstract class CliffHangerMixin extends Hanger {
         return levelAccessor.getBlockState(blockPos.below()).is(BlockTags.NETHER_CARVER_REPLACEABLES)
                 && levelAccessor.getBlockState(blockPos.below()).isValidSpawn(levelAccessor, blockPos, entityType)
                 && levelAccessor.getDifficulty() != Difficulty.PEACEFUL;
+    }
+
+    @Inject(method = "hurt", at = @At("TAIL"), remap = true)
+    private void tfg$hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        setTarget(null);
+        deactivateTongue();
+        setCantAttackTicks(20);
     }
 }
