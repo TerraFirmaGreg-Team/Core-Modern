2ebe8b08269483594e9a6aae4f9ab9c51b4272f0
Pyritie <pyritie@gmail.com>
2026-06-20 13:57:10 +0100

    make spectres fire immune

--- src/main/java/su/terrafirmagreg/core/mixins/common/species/SpectreMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/species/SpectreMixin.java
@@ -62,8 +62,8 @@ public abstract class SpectreMixin extends Monster {
         this.getAttribute(Attributes.FOLLOW_RANGE).setBaseValue(16);
     }
 
-	@Override
-	public boolean fireImmune() {
-		return true;
-	}
+    @Override
+    public boolean fireImmune() {
+        return true;
+    }
 }
