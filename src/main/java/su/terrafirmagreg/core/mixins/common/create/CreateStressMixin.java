f6a50ca7575bd9e668d8a7c98dfd0f99399d0fc9
Redeix <brayden.j.m.ford@gmail.com>
2026-07-24 14:06:07 -0500

    stressful

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/create/CreateStressMixin.java
@@ -0,0 +1,21 @@
+package su.terrafirmagreg.core.mixins.common.create;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.simibubi.create.api.stress.BlockStressValues;
+
+import net.dries007.tfc.common.blocks.devices.BellowsBlock;
+import net.dries007.tfc.common.blocks.devices.QuernBlock;
+import net.minecraft.world.level.block.Block;
+
+@Mixin(value = BlockStressValues.class, remap = false)
+public abstract class CreateStressMixin {
+    @Inject(method = "getImpact", at = @At("HEAD"), cancellable = true)
+    private static void onGetImpact(Block block, CallbackInfoReturnable<Double> cir) {
+        if (block instanceof QuernBlock) cir.setReturnValue(1.0);
+        if (block instanceof BellowsBlock) cir.setReturnValue(0.5);
+    }
+}
---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/create/KineticStatsMixin.java
@@ -0,0 +1,28 @@
+package su.terrafirmagreg.core.mixins.common.create;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.simibubi.create.content.kinetics.base.IRotate;
+import com.simibubi.create.content.kinetics.base.KineticBlock;
+import com.simibubi.create.foundation.item.KineticStats;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+@Mixin(value = KineticStats.class, remap = false)
+public abstract class KineticStatsMixin {
+
+    @Inject(method = "create", at = @At("HEAD"), cancellable = true)
+    private static void onCreate(Item item, CallbackInfoReturnable<KineticStats> cir) {
+        if (item instanceof BlockItem blockItem) {
+            Block block = blockItem.getBlock();
+            if (block instanceof IRotate && !(block instanceof KineticBlock)) {
+                cir.setReturnValue(new KineticStats(block));
+            }
+        }
+    }
+}
