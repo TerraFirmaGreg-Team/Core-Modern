0ab34b2461c0154d6e37df76365df1d4b36f3cb8
Sakura <sakura.kitsurugi@proton.me>
2026-07-26 19:38:24 +0300

    all good now

--- src/main/java/su/terrafirmagreg/core/common/data/TFGSounds.java
+++ src/main/java/su/terrafirmagreg/core/common/data/TFGSounds.java
@@ -6,50 +6,23 @@ import java.util.function.Supplier;
 import com.gregtechceu.gtceu.api.sound.SoundEntry;
 
 import net.dries007.tfc.client.TFCSounds;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 import su.terrafirmagreg.core.TFGCore;
 
 @SuppressWarnings({ "unchecked" })
 public final class TFGSounds {
-    public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, TFGCore.MOD_ID);
-
-    public static final TFCSounds.EntitySound SEAL = createTfcEntitySounds("seal", true, false);
-    public static final TFCSounds.EntitySound BISON = createTfcEntitySounds("bison", true, false);
+    public static final TFCSounds.EntitySound SEAL = new TFCSounds.EntitySound(() -> SoundEvents.FOX_AMBIENT, () -> SoundEvents.FOX_DEATH, () -> SoundEvents.FOX_HURT, () -> SoundEvents.CHICKEN_STEP,
+            Optional.of((Supplier) () -> SoundEvents.FOX_BITE), Optional.of((Supplier) () -> SoundEvents.FOX_SLEEP));
+    public static final TFCSounds.EntitySound BISON = new TFCSounds.EntitySound(() -> SoundEvents.FOX_AMBIENT, () -> SoundEvents.FOX_DEATH, () -> SoundEvents.FOX_HURT, () -> SoundEvents.CHICKEN_STEP,
+            Optional.of((Supplier) () -> SoundEvents.FOX_BITE), Optional.of((Supplier) () -> SoundEvents.FOX_SLEEP));
     public static final TFCSounds.EntitySound FOX = new TFCSounds.EntitySound(() -> SoundEvents.FOX_AMBIENT, () -> SoundEvents.FOX_DEATH, () -> SoundEvents.FOX_HURT, () -> SoundEvents.CHICKEN_STEP,
             Optional.of((Supplier) () -> SoundEvents.FOX_BITE), Optional.of((Supplier) () -> SoundEvents.FOX_SLEEP));
+
+    public static final SoundEntry SEAL_AMBIENT = TFGCore.REGISTRATE.sound(TFGCore.id("seal_ambient")).addVariant(TFGCore.id("seal_ambient_1")).build();
 
     public static final SoundEntry GEOLOGIC_VULCANIZER = TFGCore.REGISTRATE.sound(TFGCore.id("geologic_vulcanizer")).build();
 
     public static void init() {
     }
-
-    private static RegistryObject<SoundEvent> createSound(String name) {
-        return SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(
-                ResourceLocation.fromNamespaceAndPath(TFGCore.MOD_ID, name)));
-    }
-
-    private static Optional<Supplier<SoundEvent>> createOptionalSound(String name, boolean isPresent) {
-        return isPresent ? Optional.of(createSound(name)) : Optional.empty();
-    }
-
-    //spotless:off
-    private static TFCSounds.EntitySound createTfcEntitySounds(String name, boolean attack, boolean sleep)
-    {
-        return new TFCSounds.EntitySound(
-                createSound("entity.%s.ambient".formatted(name)),
-                createSound("entity.%s.death".formatted(name)),
-                createSound("entity.%s.hurt".formatted(name)),
-                createSound("entity.%s.step".formatted(name)),
-                createOptionalSound("entity.%s.attack".formatted(name), attack),
-                createOptionalSound("entity.%s.sleep".formatted(name), sleep)
-        );
-    }
-    //spotless:on
-
 }
