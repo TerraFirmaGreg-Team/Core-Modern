98ee33ed3fe7351374593b3656fa1d4c0e13e883
Pyritie <pyritie@gmail.com>
2026-08-04 22:14:24 +0100

    add fowlplay

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/fowlplay/CustomMobCategoryMixin.java
@@ -0,0 +1,24 @@
+package su.terrafirmagreg.core.mixins.common.fowlplay;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.entity.MobCategory;
+
+import aqario.fowlplay.common.entity.forge.CustomMobCategoryImpl;
+
+@Mixin(value = CustomMobCategoryImpl.class, priority = 1500, remap = false)
+public class CustomMobCategoryMixin {
+
+    @Inject(method = "ambientBirds", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void tfg$ambientBirds(CallbackInfoReturnable<MobCategory> cir) {
+        cir.setReturnValue(MobCategory.AMBIENT);
+    }
+
+    @Inject(method = "birds", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void tfg$birds(CallbackInfoReturnable<MobCategory> cir) {
+        cir.setReturnValue(MobCategory.AMBIENT);
+    }
+}
