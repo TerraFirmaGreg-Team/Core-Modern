15523f9cf879cbab19a42d8bd5f8ff83507c1096
Redeix <brayden.j.m.ford@gmail.com>
2026-08-19 14:19:36 -0500

    fix rendering? it works at least idk

--- src/main/java/su/terrafirmagreg/core/mixins/client/tfc/render/BellowsBlockEntityRendererMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/client/tfc/render/BellowsBlockEntityRendererMixin.java
@@ -1,7 +1,6 @@
 package su.terrafirmagreg.core.mixins.client.tfc.render;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -25,16 +24,15 @@ import su.terrafirmagreg.core.common.data.blocks.TFGBlocks_Girders;
  * Mixin into {@link BellowsBlockEntityRenderer} to add render handling for Create rotation support.
  * Also fixes the infamous stretching bug.
  */
-@Pseudo
 @Mixin(value = BellowsBlockEntityRenderer.class, remap = false)
 public class BellowsBlockEntityRendererMixin {
 
-    @ModifyVariable(method = "drawMiddle", at = @At(value = "STORE"), name = "change", remap = false)
+    @ModifyVariable(method = "drawMiddle", at = @At(value = "STORE"), name = "change")
     private float tfg$fixBellowsStretching(float change) {
         return Math.min(change, 0.375f);
     }
 
-    @Inject(method = "render*", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V", remap = true))
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V", remap = true))
     private void tfg$renderAxle(BellowsBlockEntity bellows, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay, CallbackInfo ci) {
         if (bellows.isConnectedToNetwork()) {
             Block axle = ForgeRegistries.BLOCKS.getValue(TFGBlocks_Girders.BRASS_BEAM.getId());
