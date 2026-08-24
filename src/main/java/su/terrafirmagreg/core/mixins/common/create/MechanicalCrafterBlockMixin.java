2cbbf2e0d2b79383028f5ab40c1ff41fbebe4a0e
Redeix <brayden.j.m.ford@gmail.com>
2026-08-24 17:37:42 -0500

    ae2 casing (#565)
    * someone didnt spotless smh
    
    * ae2 casing

--- src/main/java/su/terrafirmagreg/core/mixins/common/create/MechanicalCrafterBlockMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/create/MechanicalCrafterBlockMixin.java
@@ -1,6 +1,5 @@
 package su.terrafirmagreg.core.mixins.common.create;
 
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
