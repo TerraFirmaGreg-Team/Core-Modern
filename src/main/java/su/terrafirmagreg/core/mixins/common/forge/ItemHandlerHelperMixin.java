85d61d4580a003e281fc35fef656970b2ad56081
ashlen <thesadru@gmail.com>
2026-09-14 22:55:02 +0200

    fix regression

--- src/main/java/su/terrafirmagreg/core/mixins/common/forge/ItemHandlerHelperMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/forge/ItemHandlerHelperMixin.java
@@ -10,27 +10,33 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.gregtechceu.gtceu.api.item.IGTTool;
 import com.gregtechceu.gtceu.api.item.tool.ToolHelper;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.ItemHandlerHelper;
 
-/** Overrides the check whether two items are equal to work with gt tools and sns sacks, this is required for create logistics. */ 
+import mod.traister101.sns.common.items.ContainerItem;
+
+/** Overrides the check whether two items are equal to work with gt tools and sns sacks, this is required for create logistics. */
 @Mixin(value = ItemHandlerHelper.class)
 public abstract class ItemHandlerHelperMixin {
     @Inject(method = "canItemStacksStack(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z", at = @At("HEAD"), cancellable = true, remap = false)
     private static void tfg$relaxedStacking(ItemStack a, ItemStack b, CallbackInfoReturnable<Boolean> cir) {
-        if (a.isEmpty() || b.isEmpty() || a.getItem() != b.getItem() || a.getMaxStackSize() != 1 || b.getMaxStackSize() != 1 || a.getDamageValue() != b.getDamageValue() || !a.areCapsCompatible(b))
+        if (!(a.getItem() instanceof IGTTool || a.getItem() instanceof ContainerItem))
+            return;
+        if (a.isEmpty() || b.isEmpty() || a.getItem() != b.getItem() || a.getMaxStackSize() != 1 || b.getMaxStackSize() != 1
+                || a.getDamageValue() != b.getDamageValue() || !a.areCapsCompatible(b) || !Objects.equals(a.getEnchantmentTags(), b.getEnchantmentTags()))
             return;
 
         // GT.Tool is lazily generated, we want to only check durability and behaviors (e.g. drill range)
         if (a.getItem() instanceof IGTTool) {
-            if (Objects.equals(a.getTagElement(ToolHelper.BEHAVIOURS_TAG_KEY), b.getTagElement(ToolHelper.BEHAVIOURS_TAG_KEY)))
+            CompoundTag ta = a.getTagElement(ToolHelper.BEHAVIOURS_TAG_KEY);
+            CompoundTag tb = b.getTagElement(ToolHelper.BEHAVIOURS_TAG_KEY);
+            if (((ta == null || ta.isEmpty()) && (tb == null || tb.isEmpty())) || Objects.equals(ta, tb))
                 cir.setReturnValue(true);
-            return;
         }
 
-        // SNS Sacks store data in the share tag instead of nbt data so we need to fall back to that
-        if (Objects.equals(a.getTag(), b.getTag()) || Objects.equals(a.getShareTag(), b.getShareTag())) {
+        // SNS Sacks send data in the share tag but not in the main nbt data
+        if (a.getItem() instanceof ContainerItem && Objects.equals(a.getShareTag(), b.getShareTag()))
             cir.setReturnValue(true);
-        }
     }
 }
