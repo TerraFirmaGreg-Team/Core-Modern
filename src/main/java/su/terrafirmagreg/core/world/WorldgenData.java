9308bbe718dc9035e837391bbbfb9edbd02d96f7
Pyritie <pyritie@gmail.com>
2026-09-15 22:15:00 +0100

    change heightBlend back to MM defaults

--- src/main/java/su/terrafirmagreg/core/world/WorldgenData.java
+++ src/main/java/su/terrafirmagreg/core/world/WorldgenData.java
@@ -29,10 +29,10 @@ public class WorldgenData extends SavedData {
             "low", 384, 384, 180, 1.0, 4, 4, 0.5,
             1.0, true, 1.0, 1.0, 0.5);
     public static final MountainScaling MOUNTAIN_SCALING_MEDIUM = new MountainScaling(
-            "medium", 704, 704, 200, 1.1, 6, 4, 0.7,
+            "medium", 704, 704, 200, 1.1, 6, 10, 0.7,
             1.5, true, 3.0, 1.0, 1.0);
     public static final MountainScaling MOUNTAIN_SCALING_HIGH = new MountainScaling(
-            "high", 1024, 1024, 230, 1.2, 8, 4, 0.85,
+            "high", 1024, 1024, 230, 1.2, 8, 16, 0.85,
             2.0, true, 5.0, 1.0, 1.0);
     public static final MountainScaling MOUNTAIN_SCALING_NEW_WORLD_DEFAULT = MOUNTAIN_SCALING_LOW;
 
