dbf61382092588deaf60d0b82c76deff08fde2e3
Pyritie <pyritie@gmail.com>
2026-09-26 12:47:09 +0100

    merge

--- src/main/java/net/dries007/tfc/util/tracker/WeatherHelpers.java
+++ src/main/java/net/dries007/tfc/util/tracker/WeatherHelpers.java
@@ -14,11 +14,8 @@ import java.util.*;
 import net.dries007.tfc.mixin.accessor.PoiSectionAccessor;
 import net.dries007.tfc.mixin.accessor.SectionStorageAccessor;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.core.SectionPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ClientboundGameEventPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
@@ -49,11 +46,9 @@ import net.dries007.tfc.common.blocks.TFCBlocks;
 import net.dries007.tfc.common.blocks.ThinSpikeBlock;
 import net.dries007.tfc.common.blocks.plant.KrummholzBlock;
 import net.dries007.tfc.config.TFCConfig;
-import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.calendar.Calendars;
 import net.dries007.tfc.util.climate.ClimateModel;
 import net.dries007.tfc.world.chunkdata.ChunkData;
-import net.minecraft.world.phys.Vec2;
 
 /**
  * Handler for custom weather and weather effects.
@@ -77,9 +72,6 @@ public final class WeatherHelpers
 	private static final int TICKS_PER_SNOW_MELT_PER_SNOW_ACCUMULATION = 3;
 	private static final int TICKS_PER_SNOW_MELT = TICKS_PER_SNOW_ACCUMULATION * TICKS_PER_SNOW_MELT_PER_SNOW_ACCUMULATION;
 
-	private static final int WIND_KMS_FACTOR = 115;
-	private static final int WIND_MS_FACTOR = 32;
-
 	// For fast forwarding, the number of "fast-forward" ticks that should be simulated for a given hour of either estimated
 	// melting, or estimated snow accumulation.
 	private static final int UPDATES_PER_SNOW_MELT_SKIP = 1 + 4_000 / TICKS_PER_SNOW_MELT;
@@ -89,9 +81,6 @@ public final class WeatherHelpers
 	private static final int MAX_UPDATES_PER_TICK = TFCConfig.SERVER.snowMaxAccumulationOnUpdate.get();
 
 
-	/**
-	 * Called before {@link ServerLevel#advanceWeatherCycle()}
-	 */
 	public static void preAdvancedWeatherCycle(ServerLevel level)
 	{
 		if (level.dimensionType().hasSkyLight() && level.getGameRules().getBoolean(GameRules.RULE_WEATHER_CYCLE) && level.getLevelData() instanceof ServerLevelData serverLevelData)
@@ -123,153 +112,6 @@ public final class WeatherHelpers
 			}
 		}
 	}
-
-	/**
-	 * This is a mapped and documented version of {@link ServerLevel#advanceWeatherCycle()}. It's not used, just kept here for reference.
-	 */
-	@SuppressWarnings("unused")
-	private static void advanceWeatherCycleVanillaImplementation(ServerLevel level, ServerLevelData serverLevelData)
-	{
-		// Called every tick
-		boolean isRaining = level.isRaining();
-		if (level.dimensionType().hasSkyLight())
-		{
-			if (level.getGameRules().getBoolean(GameRules.RULE_WEATHER_CYCLE))
-			{
-				int weatherClearTime = serverLevelData.getClearWeatherTime(); // Ticks that the weather will remain clear for
-				int thunderTime = serverLevelData.getThunderTime();
-				int rainTime = serverLevelData.getRainTime();
-
-				boolean isCurrentlyThundering = serverLevelData.isThundering();
-				boolean isCurrentlyRaining = serverLevelData.isRaining();
-
-				if (weatherClearTime > 0)
-				{
-					// If clear, set to default values for clear weather
-					weatherClearTime--;
-					thunderTime = isCurrentlyThundering ? 0 : 1;
-					rainTime = isCurrentlyRaining ? 0 : 1;
-					isCurrentlyThundering = false;
-					isCurrentlyRaining = false;
-				}
-				else
-				{
-					// Not clear - there are zero ticks of weather clear time
-					if (thunderTime > 0)
-					{
-						// It's currently thundering? or are these just ticks spent waiting for thunder
-						thunderTime--; // Count down thundering ticks
-						if (thunderTime == 0)
-						{
-							// When we reach zero, swap thundering and non-thundering
-							isCurrentlyThundering = !isCurrentlyThundering;
-						}
-					}
-					else if (isCurrentlyThundering)
-					{
-						// When it's thundering but no thunder time, we pick a random duration for it to thunder for
-						// (thundering, 0 ticks) -> (thundering, [3600, 15600] ticks)
-						// (thundering, >0 ticks) -> stays the same
-						thunderTime = Mth.randomBetweenInclusive(level.random, 3600, 15600);
-					}
-					else
-					{
-						// If it's *NOT* thundering, and thunder time < 0, we pick a random LONG time
-						// This is the time UNTIL it will start thundering again.
-						thunderTime = Mth.randomBetweenInclusive(level.random, 12000, 180000);
-					}
-
-					if (rainTime > 0)
-					{
-						// Does the same with rain, always counts down if there is a counter to be counted
-						rainTime--;
-						if (rainTime == 0)
-						{
-							// If we reach zero, swaps the current setup
-							isCurrentlyRaining = !isCurrentlyRaining;
-						}
-					}
-					else if (isCurrentlyRaining)
-					{
-						// If we're at zero and RAINING, then we need to start raining for a shorter duration
-						rainTime = Mth.randomBetweenInclusive(level.random, 12000, 24000);
-					}
-					else
-					{
-						// If we're at zero and NOT RAINING, then we need to start a LONG timer
-						rainTime = Mth.randomBetweenInclusive(level.random, 12000, 180000);
-					}
-				}
-
-				// Update the data
-				serverLevelData.setThunderTime(thunderTime);
-				serverLevelData.setRainTime(rainTime);
-				serverLevelData.setClearWeatherTime(weatherClearTime);
-				serverLevelData.setThundering(isCurrentlyThundering);
-				serverLevelData.setRaining(isCurrentlyRaining);
-			}
-
-			// Linearly interpolation
-			// o = Old
-			// Change actual thunder level by 0.01 each tick based on if we are actually thundering or not
-			level.oThunderLevel = level.thunderLevel;
-			if (serverLevelData.isThundering())
-			{
-				level.thunderLevel += 0.01F;
-			}
-			else
-			{
-				level.thunderLevel -= 0.01F;
-			}
-			level.thunderLevel = Mth.clamp(level.thunderLevel, 0.0F, 1.0F);
-
-			// Do the same with rain
-			level.oRainLevel = level.rainLevel;
-			if (serverLevelData.isRaining())
-			{
-				level.rainLevel += 0.01F;
-			}
-			else
-			{
-				level.rainLevel -= 0.01F;
-			}
-
-			level.rainLevel = Mth.clamp(level.rainLevel, 0.0F, 1.0F);
-
-			// rainLevel and thunderLevel are now nicely clamped between [0, 1], and smoothly interpolating between values
-			// Note when accessing, getThunderLevel() returns thunderLevel * rainLevel;
-			// So if it's thundering but not raining, then it won't be doing either
-		}
-
-		if (level.oRainLevel != level.rainLevel)
-		{
-			level.getServer().getPlayerList().broadcastAll(new ClientboundGameEventPacket(ClientboundGameEventPacket.RAIN_LEVEL_CHANGE, level.rainLevel), level.dimension());
-		}
-
-		if (level.oThunderLevel != level.thunderLevel)
-		{
-			level.getServer().getPlayerList().broadcastAll(new ClientboundGameEventPacket(ClientboundGameEventPacket.THUNDER_LEVEL_CHANGE, level.thunderLevel), level.dimension());
-		}
-
-		/* The function in use here has been replaced in order to only send the weather info to players in the correct dimension,
-		 * rather than to all players on the server. This is what causes the client-side rain, as the
-		 * client believes that it has started raining locally, rather than in another dimension.
-		 */
-		if (isRaining != level.isRaining())
-		{
-			if (isRaining)
-			{
-				level.getServer().getPlayerList().broadcastAll(new ClientboundGameEventPacket(ClientboundGameEventPacket.STOP_RAINING, 0.0F), level.dimension());
-			}
-			else
-			{
-				level.getServer().getPlayerList().broadcastAll(new ClientboundGameEventPacket(ClientboundGameEventPacket.START_RAINING, 0.0F), level.dimension());
-			}
-
-			level.getServer().getPlayerList().broadcastAll(new ClientboundGameEventPacket(ClientboundGameEventPacket.RAIN_LEVEL_CHANGE, level.rainLevel), level.dimension());
-			level.getServer().getPlayerList().broadcastAll(new ClientboundGameEventPacket(ClientboundGameEventPacket.THUNDER_LEVEL_CHANGE, level.thunderLevel), level.dimension());
-		}
-    }
 
 	/**
 	 * Converts wind speed to M/s
@@ -878,167 +720,5 @@ public final class WeatherHelpers
 	public static boolean isIce(BlockState state)
 	{
 		return state.getBlock() == Blocks.ICE || state.getBlock() == TFCBlocks.ICE_PILE.get() || state.getBlock() == TFCBlocks.SEA_ICE.get();
-	}
-
-	/**
-	 * Converts wind speed to M/s
-	 */
-	public static float windMS(Vec2 wind)
-	{
-		return wind.length() * WIND_MS_FACTOR;
-	}
-
-	/**
-	 * Converts wind speed to M/tick (useful for accurately affecting entity/particle velocity)
-	 */
-	public static float windMT(Vec2 wind)
-	{
-		return wind.length() * WIND_MS_FACTOR / 20;
-	}
-
-	/**
-	 * Converts wind speed to KM/h
-	 */
-	public static float windKMH(Vec2 wind)
-	{
-		return wind.length() * WIND_KMS_FACTOR;
-	}
-
-	/**
-	 * Wraps and makes a wind angle positive, implicit that with the resulting angle, north is 0 degrees
-	 */
-	public static float wrappedPositiveAngle(float angleIn)
-	{
-		float angle = angleIn < 0
-						  ? angleIn += Mth.TWO_PI
-						  : angleIn;
-		// rotate so North is signal 0/15
-		angle += Mth.PI / 2;
-		// wrap
-		if (angle > Mth.TWO_PI)
-		{
-			angle -= Mth.TWO_PI;
-		}
-		return angle;
-	}
-
-	/**
-	 * Table for getting the wind direction as a cardinal, represented as an int
-	 * Should be used in all cases to ensure consistency between any cardinal direction representation of a wind angle
-	 */
-	public static int granularCardinalIntFromAngle(float angle)
-	{
-		angle *= Mth.RAD_TO_DEG;
-		float m = 11.25f;
-
-		// implicit north
-		int direction = 0;
-
-		if (angle <= 22.5 + m && angle >= 22.5 - m)
-		{
-			// north by northeast
-			direction = 1;
-		}
-		else if (angle <= 45 + m && angle >= 45 - m)
-		{
-			// northeast
-			direction = 2;
-		}
-		else if (angle <= 67.5 + m && angle >= 67.5 - m)
-		{
-			// east by northeast
-			direction = 3;
-		}
-		else if (angle <= 90 + m && angle >= 90 - m)
-		{
-			// east
-			direction = 4;
-		}
-		else if (angle <= 112.5 + m && angle >= 112.5 - m)
-		{
-			// east by southeast
-			direction = 5;
-		}
-		else if (angle <= 135 + m && angle >= 135 - m)
-		{
-			// southeast
-			direction = 6;
-		}
-		else if (angle <= 157.5 + m && angle >= 157.5 - m)
-		{
-			// south by southeast
-			direction = 7;
-		}
-		else if (angle <= 180 + m && angle >= 180 - m)
-		{
-			// south
-			direction = 8;
-		}
-		else if (angle <= 202.5 + m && angle >= 202.5 - m)
-		{
-			// south by southwest
-			direction = 9;
-		}
-		else if (angle <= 225 + m && angle >= 225 - m)
-		{
-			// southwest
-			direction = 10;
-		}
-		else if (angle <= 247.5 + m && angle >= 247.5 - m)
-		{
-			// west by southwest
-			direction = 11;
-		}
-		else if (angle <= 270 + m && angle >= 270 - m)
-		{
-			// west
-			direction = 12;
-		}
-		else if (angle <= 292.5 + m && angle >= 292.5 - m)
-		{
-			// west by northwest
-			direction = 13;
-		}
-		else if (angle <= 315 + m && angle >= 315 - m)
-		{
-			// northwest
-			direction = 14;
-		}
-		else if (angle <= 337.5 + m && angle >= 337.5 - m)
-		{
-			// north by northwest
-			direction = 15;
-		}
-
-		return direction;
-	}
-
-	/**
-	 * Returns the appropriate cardinal direction translation for any wind angle
-	 */
-	public static Component windGranularCardinal(Vec2 wind)
-	{
-		final float angle = wrappedPositiveAngle((float) Mth.atan2(wind.y, wind.x));
-		int direction = granularCardinalIntFromAngle(angle);
-		switch (direction)
-		{
-			case 0 -> {return Helpers.translateEnum(Direction.NORTH);}
-			case 1 -> {return Component.translatable("tfc.direction.cardinal_granular", Helpers.translateEnum(Direction.NORTH), Component.translatable("tfc.direction.cardinal_northeast"));}
-			case 2 -> {return Component.translatable("tfc.direction.cardinal_northeast");}
-			case 3 -> {return Component.translatable("tfc.direction.cardinal_granular", Helpers.translateEnum(Direction.EAST), Component.translatable("tfc.direction.cardinal_northeast"));}
-			case 4 -> {return Helpers.translateEnum(Direction.EAST);}
-			case 5 -> {return Component.translatable("tfc.direction.cardinal_granular", Helpers.translateEnum(Direction.EAST), Component.translatable("tfc.direction.cardinal_southeast"));}
-			case 6 -> {return Component.translatable("tfc.direction.cardinal_southeast");}
-			case 7 -> {return Component.translatable("tfc.direction.cardinal_granular", Helpers.translateEnum(Direction.SOUTH), Component.translatable("tfc.direction.cardinal_southeast"));}
-			case 8 -> {return Helpers.translateEnum(Direction.SOUTH);}
-			case 9 -> {return Component.translatable("tfc.direction.cardinal_granular", Helpers.translateEnum(Direction.SOUTH), Component.translatable("tfc.direction.cardinal_southwest"));}
-			case 10 -> {return Component.translatable("tfc.direction.cardinal_southwest");}
-			case 11 -> {return Component.translatable("tfc.direction.cardinal_granular", Helpers.translateEnum(Direction.WEST), Component.translatable("tfc.direction.cardinal_southwest"));}
-			case 12 -> {return Helpers.translateEnum(Direction.WEST);}
-			case 13 -> {return Component.translatable("tfc.direction.cardinal_granular", Helpers.translateEnum(Direction.WEST), Component.translatable("tfc.direction.cardinal_northwest"));}
-			case 14 -> {return Component.translatable("tfc.direction.cardinal_northwest");}
-			case 15 -> {return Component.translatable("tfc.direction.cardinal_granular", Helpers.translateEnum(Direction.NORTH), Component.translatable("tfc.direction.cardinal_northwest"));}
-		}
-		return Component.empty();
 	}
 }
